c36135de352ee1597a15824a3388fcbde236d763
Juan Lulkin <juan.lulkin@klarna.com>
2016-01-26 13:00:12 +0200

    Remove meta-specific classes

--- example/Buttons.jsx
+++ example/Buttons.jsx
@@ -3,30 +3,30 @@ import Button from '../components/Button'
 
 export default function Buttons () {
   return (
-    <div className='button-examples'>
-      <div className='appchrome__section appchrome__section--bottomline'>
+    <div>
+      <div>
         <h1>Button examples</h1>
       </div>
 
-      <div className='appchrome__section'>
+      <div>
         <Button size='small'>Click me!</Button>
         <Button size='normal'>Click me!</Button>
         <Button size='big'>Click me!</Button>
       </div>
 
-      <div className='appchrome__section'>
+      <div>
         <Button design='secondary' size='small'>Click me!</Button>
         <Button design='secondary' size='normal'>Click me!</Button>
         <Button design='secondary' size='big'>Click me!</Button>
       </div>
 
-      <div className='appchrome__section'>
+      <div>
         <Button loading size='small'>Click me!</Button>
         <Button loading size='normal'>Click me!</Button>
         <Button loading size='big'>Click me!</Button>
       </div>
 
-      <div className='appchrome__section'>
+      <div>
         <Button success size='small'>Click me!</Button>
         <Button success size='normal'>Click me!</Button>
         <Button success size='big'>Click me!</Button>
