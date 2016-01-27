25977e41f115e658c9c3560da6715bebcc305bde
Juan Lulkin <juan.lulkin@klarna.com>
2016-01-27 11:38:11 +0200

    WIP

---
+++ components/Loader.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react'
+import classnames from 'classnames'
+import styles from 'ui-toolkit/src/components/loader.scss'
+
+export default function Loader ({className, small}) {
+  const cls = classnames(className, {
+    'cui__loader': !small,
+    'cui__loader--small': small
+  })
+
+  return <div className={ cls }></div>
+}
+
+Loader.propTypes = {
+  className: PropTypes.string,
+  small: PropTypes.bool
+}
