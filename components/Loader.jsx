000dcdc97ceb196fc501ce4b1bd42e37a14c20b9
Juan Lulkin <maiz@lulk.in>
2016-02-10 14:24:56 +0200

    Update ui-css-components path

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import classnames from 'classnames'
-import styles from 'ui-toolkit/src/components/loader.scss'
+import styles from 'ui-css-components/src/components/loader.scss'
 
 export default function Loader ({className, small}) {
   const cls = classnames(className, {
