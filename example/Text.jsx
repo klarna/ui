aa570fcb40ea3ddc3f89efdb600d95d702aff0c1
Matthias Feist <matthias.feist@klarna.com>
2016-02-23 16:28:45 +0100

    removed linter errors

--- example/Text.jsx
+++ example/Text.jsx
@@ -8,28 +8,28 @@ export default function Text () {
 
       <Code>
         <PrimaryTitle>Primary Title</PrimaryTitle>
-        <PrimaryTitle blue={true}>Primary Title in blue</PrimaryTitle>
-        <PrimaryTitle small={true}>Primary Title in small</PrimaryTitle>
-        <PrimaryTitle strong={true} small={true}>Primary Title in strong</PrimaryTitle>
+        <PrimaryTitle blue>Primary Title in blue</PrimaryTitle>
+        <PrimaryTitle small>Primary Title in small</PrimaryTitle>
+        <PrimaryTitle strong small>Primary Title in strong</PrimaryTitle>
       </Code>
 
       <Code>
         <SecondaryTitle>Secondary Title</SecondaryTitle>
-        <SecondaryTitle blue={true}>Secondary Title in blue</SecondaryTitle>
-        <SecondaryTitle condensed={true}>Secondary Title in condensed</SecondaryTitle>
+        <SecondaryTitle blue>Secondary Title in blue</SecondaryTitle>
+        <SecondaryTitle condensed>Secondary Title in condensed</SecondaryTitle>
       </Code>
 
       <Code>
         <Subtitle>Subtitle</Subtitle>
-        <Subtitle blue={true}>Subtitle in blue</Subtitle>
-        <Subtitle condensed={true}>Subtitle in condensed</Subtitle>
+        <Subtitle blue>Subtitle in blue</Subtitle>
+        <Subtitle condensed>Subtitle in condensed</Subtitle>
       </Code>
 
       <Code>
         <Paragraph>Primary Paragraph Primary Paragraph Primary Paragraph Primary Paragraph Primary Paragraph </Paragraph>
-        <Paragraph condensed={true}>Primary Paragraph Condensed Primary Paragraph Condensed Primary Paragraph Condensed Primary Paragraph Condensed </Paragraph>
-        <Paragraph design="secondary">Secondary Paragraph Secondary Paragraph Secondary Paragraph Secondary Paragraph Secondary Paragraph </Paragraph>
-        <Paragraph design="legal">Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text </Paragraph>
+        <Paragraph condensed>Primary Paragraph Condensed Primary Paragraph Condensed Primary Paragraph Condensed Primary Paragraph Condensed </Paragraph>
+        <Paragraph design='secondary'>Secondary Paragraph Secondary Paragraph Secondary Paragraph Secondary Paragraph Secondary Paragraph </Paragraph>
+        <Paragraph design='legal'>Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text Legal Text </Paragraph>
       </Code>
 
       <Code>
