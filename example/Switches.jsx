8c7ee9ca7f12076c8cb5f15c84230a2b44e9800a
Juan Lulkin <maiz@lulk.in>
2016-03-08 18:08:19 +0200

    Adds Switch examples

---
+++ example/Switches.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Switch from '../components/Switch'
+import Button from '../components/Button'
+import Code from './Code'
+
+export default function Switches () {
+  return (
+    <div>
+      <h1>Switch examples</h1>
+
+      <h2>Primary</h2>
+
+      <h5>Regular</h5>
+      <Code>
+        <Switch>This is a toggle switch</Switch>
+      </Code>
+
+      <h5>Checked</h5>
+      <Code>
+        <Switch checked>This is a toggle switch checked</Switch>
+      </Code>
+
+      <h5>Right</h5>
+      <Code>
+        <Switch align='right'>This is a toggle switch on the right side</Switch>
+      </Code>
+
+      <h5>Disabled</h5>
+      <Code>
+        <Switch disabled>This is a toggle switch disabled</Switch>
+      </Code>
+
+      <h5>Checkbox</h5>
+      <Code>
+        <Switch design='checkbox'>This is a toggle switch with checkbox design</Switch>
+      </Code>
+
+      <h5>In a form</h5>
+      <Code>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          window.alert(event.target.fries.value)
+        }}>
+          <Switch name='fries'>Would you like fries?</Switch>
+          <Button size='small'>Try me</Button>
+        </form>
+      </Code>
+
+      <h5>Outside a form (using a callback)</h5>
+      <Code>
+        <Switch onChange={(value) => {
+          window.alert(value)
+        }}>Would you like fries?</Switch>
+      </Code>
+
+    </div>
+  )
+}
