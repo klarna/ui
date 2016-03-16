cb62a24c93482df6db9ee7ae763ce9d230993367
Juan Lulkin <juan.lulkin@klarna.com>
2016-03-16 11:38:04 +0200

    Adds Tooltip component

---
+++ components/Tooltip.jsx
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react'
+import classNamesBind from 'classnames/bind'
+import styles from '@klarna/ui-css-components/src/components/tooltip.scss'
+
+const classNames = classNamesBind.bind(styles)
+
+export default function Tooltip ({className, arrow, children}) {
+  const cls = classNames('cui__tooltip', arrow, className)
+
+  return (
+    <div className={cls}>
+      {children}
+    </div>
+  )
+}
+
+Tooltip.arrows = [
+  'top', 'top-left', 'top-right',
+  'bottom', 'bottom-left', 'bottom-right',
+  'left', 'left-top', 'left-bottom',
+  'right', 'right-top', 'right-bottom'
+]
+
+Tooltip.propTypes = {
+  className: PropTypes.string,
+  arrow: PropTypes.oneOf(Tooltip.arrows),
+  children: PropTypes.node
+}
