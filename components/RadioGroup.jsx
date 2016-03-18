9f9bf39a3c1662e24845c1a25c32ca2a3ec06d6f
Juan Lulkin <maiz@lulk.in>
2016-03-18 16:45:43 +0200

    Adds RadioGroup

---
+++ components/RadioGroup.jsx
@@ -0,0 +1,46 @@
+import React, { PropTypes } from 'react'
+import classNamesBind from 'classnames/bind'
+import styles from '@klarna/ui-css-components/src/components/dropdown.scss'
+
+const classNames = classNamesBind.bind(styles)
+
+export default function RadioGroup (props) {
+  const { selected, onChange, className, data, ...remainingProps } = props
+  const baseClass = 'cui__dropdown--radio'
+  const cls = classNames(baseClass, className)
+
+  const options = data.map(({id, label, description}) => {
+    const optionClass = classNames(`${baseClass}__option`, {
+      'is-selected': id === selected
+    })
+    const labelClass = classNames(`${baseClass}__option__heading`)
+    const descriptionClass = classNames(`${baseClass}__option__description`)
+
+    return (
+      <div key={id} className={optionClass} onClick={() => onChange(id)}>
+        <div className={labelClass}>{label}</div>
+        {description && <div className={descriptionClass}>{description}</div>}
+      </div>
+    )
+  })
+
+  return (
+    <div className={cls} {...remainingProps}>
+      {options}
+    </div>
+  )
+}
+
+RadioGroup.optionSchema = PropTypes.shape({
+  id: PropTypes.any.isRequired,
+  label: PropTypes.string.isRequired,
+  description: PropTypes.node
+})
+
+RadioGroup.propTypes = {
+  // Allows any type to be an id, as long as it is comparable
+  selected: React.PropTypes.any.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  className: PropTypes.string,
+  data: PropTypes.arrayOf(RadioGroup.optionSchema).isRequired
+}
