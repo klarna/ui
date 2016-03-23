ee79d76574e6e9ac46c2ea07c8daff4e9160af22
Juan Lulkin <juan.lulkin@klarna.com>
2016-03-23 19:40:04 +0200

    Finishes Link test

--- tests/Link.spec.jsx
+++ tests/Link.spec.jsx
@@ -8,20 +8,27 @@ const render = renderer(Link)
 
 describe('Link', () => {
   describe('default', () => {
-    const loader = render()
+    const link = render()
 
-    it("renders tag 'a'", () => {
-      equal('a', loader.type)
+    it('renders tag "a"', () => {
+      equal('a', link.type)
     })
 
-    it("has className 'cui__link'", () => {
-      equal('cui__link', loader.props.className)
+    it('has className "cui__link"', () => {
+      equal('cui__link', link.props.className)
     })
   })
 
-  it("allows passing custom 'className' without overriding defaults", () => {
-    const loader = render({ className: 'custom' })
+  it('allows passing custom "className" without overriding defaults', () => {
+    const link = render({ className: 'custom' })
 
-    equal('cui__link custom', loader.props.className)
+    equal('cui__link custom', link.props.className)
+  })
+
+  it('allows more props', () => {
+    const link = render({ href: 'http://test.com', onClick: () => {} })
+
+    equal('http://test.com', link.props.href)
+    equal('function', typeof link.props.onClick)
   })
 })
