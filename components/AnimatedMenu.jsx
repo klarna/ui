7dc3a008861f6a66dda4d41b853b3ad3c46e1923
Juan Lulkin <maiz@lulk.in>
2016-03-31 19:05:31 +0300

    Adds white and animated tab-menu

---
+++ components/AnimatedMenu.jsx
@@ -0,0 +1,67 @@
+import React, { PropTypes } from 'react'
+import { Motion, spring } from 'react-motion'
+import Menu from './Menu'
+import styles from '@klarna/ui-css-components/src/components/tab-menu.scss'
+
+const springConfig = { stiffness: 170, damping: 26 }
+
+const AnimatedSelectedBar = ({ width, left }) => (
+  <Motion style={{left: spring(left, springConfig), width: spring(width, springConfig)}}>
+    {(style) => <div style={style} className={styles['cui__tab-menu__selected']} />}
+  </Motion>
+)
+
+export default class AnimatedMenu extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { width: null, left: null }
+
+    this.onClick = this.onClick.bind(this)
+  }
+
+  setStateForTab (tab) {
+    const { left, width } = tab.getBoundingClientRect()
+    const parentLeft = tab.parentNode.getBoundingClientRect().left
+
+    this.setState({ width, left: (left - parentLeft) })
+  }
+
+  setStateForSelectedTab () {
+    this.setStateForTab(document.getElementById(`${this.props.name}-${this.props.selected}-tab`))
+  }
+
+  componentDidUpdate (prevProps) {
+    if (this.props.options.length !== prevProps.options.length) {
+      this.setStateForSelectedTab()
+    }
+  }
+
+  componentDidMount () {
+    setTimeout(() => {
+      this.setStateForSelectedTab()
+    }, 0)
+  }
+
+  onClick ({ target }) {
+    this.setStateForTab(target)
+  }
+
+  render () {
+    return (
+      <Menu {...this.props} design='tab' selectable={false} onClick={this.onClick}>
+        {this.state.width && <AnimatedSelectedBar {...this.state} />}
+      </Menu>
+    )
+  }
+}
+
+AnimatedMenu.propTypes = {
+  options: PropTypes.arrayOf(Menu.optionsSchema).isRequired,
+  className: PropTypes.string,
+  tabDisplay: PropTypes.oneOf(Menu.tabDisplays),
+  onChange: PropTypes.func,
+  name: PropTypes.string.isRequired,
+  selected: PropTypes.string,
+  white: PropTypes.bool
+}
