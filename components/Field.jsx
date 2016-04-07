e03a44d387f6f94d83ef3e5e00934f18fb1a394b
Fernando Via Canel <fernando.canel@klarna.com>
2016-04-07 13:24:30 +0000

    Add support for positions

---
+++ components/Field.jsx
@@ -0,0 +1,99 @@
+import React, { PropTypes } from 'react'
+import classNamesBind from 'classnames/bind'
+import styles from '@klarna/ui-css-components/src/components/field.scss'
+
+const classNames = classNamesBind.bind(styles)
+const toObjectWithValue = (value) => (list) =>
+  list.reduce((accumulator, item) => ({
+    ...accumulator,
+    [item]: value
+  }), {})
+const combinations = (xs, ys) =>
+  xs
+    .map((x) => ys.map((y) => [x, y]))
+    .reduce((a, b) => a.concat(b), [])
+
+export const positions = [
+  'bottom',
+  'left',
+  'right',
+  'top'
+]
+
+export const states = [
+  'disabled',
+  'focus',
+  'error',
+  'warning'
+]
+
+const prioritizedAllowedPositionCombinations =
+  combinations(
+    ['bottom', 'top'],
+    ['left', 'right']
+  )
+  .concat(positions.map((x) => [x]))
+  .sort((a, b) => a.length > b.length)
+
+export default function Field ({
+  big,
+  className,
+  disabled,
+  error,
+  focus,
+  label,
+  value,
+  warning,
+  ...props
+}) {
+  const positionCombination = (
+    prioritizedAllowedPositionCombinations
+      .find((combination) =>
+        combination.length === 1
+          ? props[combination[0]]
+          : (props[combination[0]] && props[combination[1]])
+      ) || []
+  ).join('-')
+
+  const classes = {
+    field: classNames(
+      'cui__field', {
+        big,
+        'is-disabled': disabled,
+        'is-error': error,
+        'is-filled': value != null && value !== '',
+        'is-focused': focus,
+        'is-warning': warning
+      },
+      positionCombination,
+      className),
+    label: classNames('cui__field__label'),
+    input: classNames('cui__field__input')
+  }
+
+  return (
+    <div className={classes.field}>
+      <label className={classes.label}>{label}</label>
+
+      <input
+        className={classes.input}
+        disabled={disabled}
+        value={value}
+      />
+    </div>
+  )
+}
+
+Field.defaultProps = {
+  big: false,
+  ...toObjectWithValue(false)(states),
+  ...toObjectWithValue(false)(positions)
+}
+
+Field.propTypes = {
+  big: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  ...toObjectWithValue(PropTypes.bool)(states),
+  ...toObjectWithValue(PropTypes.bool)(positions)
+}
---
+++ tests/Field.spec.jsx
@@ -0,0 +1,173 @@
+/* global describe it */
+
+import Field from '../components/Field'
+import { equal, ok } from 'assert'
+import { renderer } from './helpers'
+
+const render = renderer(Field)
+
+const input = (field) => field.props.children[1]
+
+describe.only('Field', () => {
+  describe('default', () => {
+    const field = render({
+      className: 'extra__className',
+      label: 'Given Name'
+    })
+
+    it("renders tag 'div'", () => {
+      equal(field.type, 'div')
+    })
+
+    it("has className 'cui__field'", () => {
+      ok(field.props.className.match('cui__field'))
+    })
+
+    describe('add a className', () => {
+      it('has the extra className', () => {
+        ok(field.props.className.match('extra__className'))
+      })
+    })
+
+    describe('label', () => {
+      const label = field.props.children[0]
+
+      it("renders tag 'label'", () => {
+        equal(label.type, 'label')
+      })
+
+      it("has className 'cui__field__label'", () => {
+        equal(label.props.className, 'cui__field__label')
+      })
+
+      it("has the label text", () => (
+        equal(label.props.children, 'Given Name')
+      ))
+    })
+
+    describe('input', () => {
+      it("renders tag 'input'", () => (
+        equal(input(field).type, 'input')
+      ))
+
+      it("has className 'cui__field__input'", () => (
+        equal(input(field).props.className, 'cui__field__input')
+      ))
+    })
+  })
+
+  describe('focused', () => {
+    const field = render({
+      label: 'Password',
+      focus: true
+    })
+
+    it("has className 'is-focused'", () => (
+      ok(field.props.className.match('is-focused'))
+    ))
+  })
+
+  describe('with value', () => {
+    const field = render({
+      label: 'Billing Address',
+      value: 'Some value'
+    })
+
+    it("has className 'is-filled'", () => (
+      ok(field.props.className.match('is-filled'))
+    ))
+
+    describe('input', () => {
+      it("has the value", () => {
+        equal(input(field).props.value, 'Some value')
+      })
+    })
+  })
+
+  describe('error', () => {
+    const field = render({
+      label: 'Something',
+      error: true
+    })
+
+    it("has className 'is-error'", () => (
+      ok(field.props.className.match('is-error'))
+    ))
+  })
+
+  describe('warning', () => {
+    const field = render({
+      label: 'Something',
+      warning: true
+    })
+
+    it("has className 'is-warning'", () => (
+      ok(field.props.className.match('is-warning'))
+    ))
+  })
+
+  describe('disabled', () => {
+    const field = render({
+      label: 'Something',
+      disabled: true
+    })
+
+    it("has className 'is-disabled'", () => (
+      ok(field.props.className.match('is-disabled'))
+    ))
+
+    describe('input', () => {
+      it("is disabled", () => (
+        ok(input(field).props.disabled)
+      ))
+    })
+  })
+
+  describe('big', () => {
+    const field = render({
+      label: 'Something',
+      big: true
+    })
+
+    it("has className 'big'", () => (
+      ok(field.props.className.match('big'))
+    ))
+  })
+
+  describe('stacked', () => {
+    describe('positions', () => {
+      [
+        'bottom',
+        'left',
+        'right',
+        'top'
+      ].forEach((position) => {
+        describe(`when in position ${position}`, () => {
+          const field = render({
+            label: `Stacked in ${position}`,
+            [position]: true
+          })
+          it(`has className '${position}'`, () => (
+            ok(field.props.className.match(position))
+          ))
+        })
+      })
+
+      return [ 'left', 'right' ].forEach((x) => {
+        [ 'bottom', 'top' ].forEach((y) => {
+          describe(`when in position ${y} ${x}`, () => {
+            const field = render({
+              label: `Stacked in ${y} ${x}`,
+              [x]: true,
+              [y]: true
+            })
+
+            it(`has className '${y}-${x}'`, () => (
+              ok(field.props.className.match(`${y}-${x}`))
+            ))
+          })
+        })
+      })
+    })
+  })
+})
