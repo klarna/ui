b0ab44dfa456cbe214177c605d7a65707221da60
Gideon Gur <gideon.gur@klarna.com>
2016-04-12 15:13:06 +0300

    tooltip with border

--- example/Tooltips.jsx
+++ example/Tooltips.jsx
@@ -69,6 +69,71 @@ export default function Tooltips () {
           Lorem Ipsum is simply dummy text of the printing industry.
         </Tooltip>
       </Code>
+
+      <h5>Simple with border</h5>
+      <Code>
+        <Tooltip border>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+      </Code>
+
+      <h5>Small</h5>
+      <Code>
+        <Tooltip border>
+          Lorem Ipsum
+        </Tooltip>
+      </Code>
+
+      <h5>Arrows</h5>
+      <Code>
+        <Tooltip border arrow='top-left'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='top'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='top-right'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='right-top'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='right'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='right-bottom'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='bottom-right'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='bottom'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='bottom-left'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='left-bottom'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='left'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+        <hr />
+        <Tooltip border arrow='left-top'>
+          Lorem Ipsum is simply dummy text of the printing industry.
+        </Tooltip>
+      </Code>
     </div>
   )
 }
