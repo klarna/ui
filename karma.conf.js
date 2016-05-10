0aa4469df2ea7724dd3e99ff2af5bccaacadd3d4
Christos Papazafeiropoulos <c.papazafeiropoulos@klarna.com>
2016-05-10 13:12:09 +0200

    removed IE8 added Android 4.3 and some packages

--- karma.conf.js
+++ karma.conf.js
@@ -52,13 +52,7 @@ module.exports = function (config) {
         browserName: 'safari',
         platform: 'OS X 10.11',
         version: '9.0'
-      },
-      SL_IE_8: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows XP',
-        version: '8.0'
-      },
+      }, 
       SL_IE_10: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
