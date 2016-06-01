d2e310d517177bc31414be054f5234bea08ebe75
Jonathan Trujillo <trujillo.jonathan@gmail.com>
2016-06-01 19:44:22 +0200

    damn

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes, Component } from 'react'
 import classNamesBind from 'classnames/bind'
-import styles from '@klarna/ui-css-components/src/components/input.scss'
+import styles from '@klarna/ui-css-components/src/components/dropdown.scss'
 import * as programmaticFocus from '../lib/features/programmaticFocus'
 import * as fieldStates from '../lib/features/fieldStates'
 import * as inlinedIcon from '../lib/features/inlinedIcon'
