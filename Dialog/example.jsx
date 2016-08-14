5ac51bb64b7e0c0466031555170e4098b78a987e
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-14 10:25:57 +0200

    Make Dialogs example work

--- Dialog/example.jsx
+++ Dialog/example.jsx
@@ -1,55 +1,60 @@
 import React, { Component } from 'react'
-import Button from '../Button'
+import * as Button from '../Button'
 import * as Dialog from '../Dialog'
-import { CloseButton } from '../IconButton'
+import { Close } from '../IconButton'
 import { Title, Subtitle, Paragraph } from '../Text'
 
-export default class Dialogs extends Component {
-  constructor () {
-    super()
+const Examples = React.createClass({
+  displayName: 'Dialogs',
 
-    this.state = {
+  getInitialState () {
+    return {
       dialog: {
         open: false
       }
     }
-  }
+  },
 
   render () {
+    const close = () => this.setState({ dialog: { open: false } })
+    const open = () => this.setState({ dialog: { open: true } })
+
     return (
       <div>
-        <Paragraph margins>
-          Dialogs are full screen. Please click the button to show it.
-        </Paragraph>
+      <Paragraph margins>
+      Dialogs are full screen. Please click the button to show it.
+      </Paragraph>
 
-        <Button onClick={() => this.setState({
-          dialog: {
-            open: true
-          }
-        })}>
-          Show Dialog
-        </Button>
+      <Button.Primary onClick={open}>
+      Show Dialog
+      </Button.Primary>
 
-        <Dialog.Overlay show={this.state.dialog.open}>
-          <Dialog.Main>
-            <Dialog.Icon>
-              <CloseButton
-                onClick={() => this.setState({ dialog: { open: false } })}
-              />
-            </Dialog.Icon>
+      <Dialog.Overlay show={this.state.dialog.open}>
+      <Dialog.Main>
+      <Dialog.Icon>
+      <Close
+      onClick={close}
+      />
+      </Dialog.Icon>
 
-            <Dialog.Content>
-              <Title.Primary margins>The title is primary</Title.Primary>
-              <Subtitle margins>Just trying to fill up space</Subtitle>
-              <Paragraph design='secondary' margins>Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.</Paragraph>
-            </Dialog.Content>
+      <Dialog.Content>
+      <Title.Primary margins>The title is primary</Title.Primary>
+      <Subtitle margins>Just trying to fill up space</Subtitle>
+      <Paragraph design='secondary' margins>Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.</Paragraph>
+      </Dialog.Content>
 
-            <Dialog.Footer>
-              <Button onClick={() => this.setState({ dialog: { open: false } })} >Close the nice dialog</Button>
-            </Dialog.Footer>
-          </Dialog.Main>
-        </Dialog.Overlay>
+      <Dialog.Footer>
+      <Button.Primary onClick={close}>
+      Close the nice dialog
+      </Button.Primary>
+      </Dialog.Footer>
+      </Dialog.Main>
+      </Dialog.Overlay>
       </div>
     )
   }
+})
+
+export default function Dialogs () {
+  return <Examples />
 }
