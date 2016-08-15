5e2b85c3a3c3fea8baed3a1a1c8455cd1c3f6606
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-15 11:30:07 +0200

    Cleanup Tooltip code

--- Tooltip/index.jsx
+++ Tooltip/index.jsx
@@ -2,27 +2,29 @@ import React, { PropTypes } from 'react'
 import classNamesBind from 'classnames/bind'
 import defaultStyles from './styles.scss'
 
-export default function Tooltip ({ className, arrow, children, border, styles }) {
-  const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
-  const cls = classNames('tooltip', arrow, className, { border })
+const baseClass = 'tooltip'
 
-  return (
-    <div className={cls}>
-      {children}
-    </div>
-  )
-}
-
-Tooltip.arrows = [
+export const arrows = [
   'top', 'top-left', 'top-right',
   'bottom', 'bottom-left', 'bottom-right',
   'left', 'left-top', 'left-bottom',
   'right', 'right-top', 'right-bottom'
 ]
 
+export default function Tooltip ({ className, arrow, children, border, styles }) {
+  const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
+
+  return (
+    <div 
+      className={classNames(baseClass, arrow, className, { border })}>
+      {children}
+    </div>
+  )
+}
+
 Tooltip.propTypes = {
   className: PropTypes.string,
-  arrow: PropTypes.oneOf(Tooltip.arrows),
+  arrow: PropTypes.oneOf(arrows),
   children: PropTypes.node,
   border: PropTypes.bool,
   styles: PropTypes.object
