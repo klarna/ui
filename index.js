2b25bcb93e17071744c566c2b5420273a9e13c40
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-25 16:39:24 +0200

    Add universal entrypoint

---
+++ index.js
@@ -0,0 +1,45 @@
+import * as TheAlert from './Alert'
+export { default as Amount } from './Amount'
+import * as TheBlock from './Block'
+import * as TheButton from './Button'
+import * as TheChecklist from './Checklist'
+export { default as Code } from './Code'
+import * as TheContextMenu from './ContextMenu'
+import * as TheDialog from './Dialog'
+export { default as Dropdown } from './Dropdown'
+export { default as Field } from './Field'
+export { default as Fieldset } from './Fieldset'
+import * as TheIconButton from './IconButton'
+import * as TheIcons from './icons'
+export { default as Input } from './Input'
+export { default as Installments } from './Installments'
+export { default as Label } from './Label'
+export { default as Link } from './Link'
+export { default as Loader } from './Loader'
+import * as TheMenu from './Menu'
+import * as TheParagraph from './Paragraph'
+import * as ThePreview from './Preview'
+export { default as ProgressBar } from './ProgressBar'
+export { default as Radio } from './Radio'
+export { default as Selector } from './Selector'
+export { default as Subtitle } from './Subtitle'
+import * as TheSwitch from './Switch'
+import * as TheText from './Text'
+export { default as TextLabel } from './TextLabel'
+import * as TheTitle from './Title'
+export { default as Tooltip } from './Tooltip'
+
+export const Alert = TheAlert
+export const Block = TheBlock
+export const Button = TheButton
+export const Checklist = TheChecklist
+export const ContextMenu = TheContextMenu
+export const Dialog = TheDialog
+export const IconButton = TheIconButton
+export const icons = TheIcons
+export const Menu = TheMenu
+export const Paragraph = TheParagraph
+export const Preview = ThePreview
+export const Switch = TheSwitch
+export const Text = TheText
+export const Title = TheTitle
