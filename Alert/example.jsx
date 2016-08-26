4f1ba97353b394b9fd3acf3c673182a93750213b
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-26 14:45:05 +0200

    Fix lint in Alert

--- Alert/example.jsx
+++ Alert/example.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
-import Code from '../Code'
-import { Subtitle } from '../Text'
 import * as Alert from '../Alert'
 
 const Error = {
   title: 'Error',
-  Regular: (<Alert.Error>
-    <Alert.Title>An error alert box heading</Alert.Title>
-    <Alert.Paragraph>Some text inside helps to get an idea of how the alert would look like. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </Alert.Paragraph>
-    <Alert.Paragraph>ID: <em>a4c531de-e35d-4901-93ae-44e32639b4b1</em></Alert.Paragraph>
-  </Alert.Error>)
+  Regular: (
+    <Alert.Error>
+      <Alert.Title>
+        An error alert box heading
+      </Alert.Title>
+      <Alert.Paragraph>
+        Some text inside helps to get an idea of how the alert would look like. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </Alert.Paragraph>
+      <Alert.Paragraph>
+        ID: <em>a4c531de-e35d-4901-93ae-44e32639b4b1</em>
+      </Alert.Paragraph>
+    </Alert.Error>
+  )
 }
 
 const Warning = {
   title: 'Warning',
-  Regular: (<Alert.Warning>
-    <Alert.Title>An error alert box heading</Alert.Title>
-    <Alert.Paragraph>Some text inside helps to get an idea of how the alert would look like. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </Alert.Paragraph>
-    <Alert.Paragraph>ID: <em>a4c531de-e35d-4901-93ae-44e32639b4b1</em></Alert.Paragraph>
-  </Alert.Warning>)
+  Regular: (
+    <Alert.Warning>
+      <Alert.Title>
+        An error alert box heading
+      </Alert.Title>
+      <Alert.Paragraph>
+        Some text inside helps to get an idea of how the alert would look like. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </Alert.Paragraph>
+      <Alert.Paragraph>
+        ID: <em>a4c531de-e35d-4901-93ae-44e32639b4b1</em>
+      </Alert.Paragraph>
+    </Alert.Warning>
+  )
 }
 
 export default {
