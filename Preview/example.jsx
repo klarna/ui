82e7720488ec6562e4dad466be93fec1b0d9b9cb
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-30 18:29:00 +0200

    Add Preview example

--- Preview/example.jsx
+++ Preview/example.jsx
@@ -1,40 +1,35 @@
 import React from 'react'
 import * as Preview from '../Preview'
-import { Title } from '../Text'
-import Code from '../Code'
 
-export default function Previews () {
-  return (
-    <div>
-      <Title.Secondary margins>Default</Title.Secondary>
-      <Code>
-        <Preview.Main>
-          <Preview.Title>John Smith</Preview.Title>
-          <Preview.Content>
-            1425 North Avenue Street<br />
-            San Francisco<br />
-            94100 California<br />
-            United States
-          </Preview.Content>
-        </Preview.Main>
-      </Code>
-
-      <Title.Secondary margins>With link</Title.Secondary>
-      <Code>
-        <Preview.Main
-          onClick={() => console.log('You clicked the address')}>
-          <Preview.Title>John Smith</Preview.Title>
-          <Preview.Content>
-            1425 North Avenue Street<br />
-            San Francisco<br />
-            94100 California<br />
-            United States
-          </Preview.Content>
-          <Preview.Link>
-            Change address
-          </Preview.Link>
-        </Preview.Main>
-      </Code>
-    </div>
-  )
+export default {
+  title: 'Preview',
+  variations: [{
+    title: 'Preview',
+    Regular: (
+      <Preview.Main>
+        <Preview.Title>John Smith</Preview.Title>
+        <Preview.Content>
+          1425 North Avenue Street<br />
+          San Francisco<br />
+          94100 California<br />
+          United States
+        </Preview.Content>
+      </Preview.Main>
+    ),
+    'With Link': (
+      <Preview.Main
+        onClick={() => console.log('You clicked the address')}>
+        <Preview.Title>John Smith</Preview.Title>
+        <Preview.Content>
+          1425 North Avenue Street<br />
+          San Francisco<br />
+          94100 California<br />
+          United States
+        </Preview.Content>
+        <Preview.Link>
+          Change address
+        </Preview.Link>
+      </Preview.Main>
+    )
+  }]
 }
