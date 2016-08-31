a9dcab7473d0d28d414a3f3764117cd11f085813
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-31 11:58:02 +0200

    Add Loader example

--- Loader/example.jsx
+++ Loader/example.jsx
@@ -1,42 +1,61 @@
 import React from 'react'
 import Loader from '../Loader'
-import { Title, Subtitle } from '../Text'
-import Code from '../Code'
+import * as Block from '../Block'
 
-export default function Loaders () {
-  return (
-    <div>
-      <Title.Secondary color='blue' margins>Primary</Title.Secondary>
-      <Code>
-        <Loader size='big' />
-        <Loader />
-        <Loader size='small' />
-        <Loader size='tiny' />
-      </Code>
-
-      <Subtitle>Inline</Subtitle>
-      <Code>
-        <Loader inline />
-        <Loader inline />
-      </Code>
-
-      <Title.Secondary color='blue' margins>Secondary</Title.Secondary>
-      <Code>
-        <Loader color='blue' size='big' />
-        <Loader color='blue' />
-        <Loader color='blue' size='small' />
-        <Loader color='blue' size='tiny' />
-      </Code>
-
-      <Title.Secondary color='blue' margins>White</Title.Secondary>
-      <Code>
-        <div style={{background: '#0074c8', padding: 20}}>
-          <Loader color='white' size='big' />
-          <Loader color='white' />
-          <Loader color='white' size='small' />
-          <Loader color='white' size='tiny' />
-        </div>
-      </Code>
-    </div>
-  )
+export default {
+  title: 'Loader',
+  variations: [{
+    title: 'Primary',
+    Big: [
+      <Loader size='big' />
+    ],
+    Regular: [
+      <Loader />
+    ],
+    Small: [
+      <Loader size='small' />
+    ],
+    Tiny: [
+      <Loader size='tiny' />
+    ],
+    Inline: [
+      <Loader inline />
+    ]
+  }, {
+    title: 'Secondary',
+    Big: [
+      <Loader color='blue' size='big' />
+    ],
+    Regular: [
+      <Loader color='blue' />
+    ],
+    Small: [
+      <Loader color='blue' size='small' />
+    ],
+    Tiny: [
+      <Loader color='blue' size='tiny' />
+    ]
+  }, {
+    title: 'White',
+    Big: [
+      <Block.Plain blue style={{padding: '20px'}}>
+        <Loader color='white' size='big' />
+      </Block.Plain>
+    ],
+    Regular: [
+      <Block.Plain blue style={{padding: '20px'}}>
+        <Loader color='white' />
+      </Block.Plain>
+    ],
+    Small: [
+      <Block.Plain blue style={{padding: '20px'}}>
+        <Loader color='white' size='small' />
+      </Block.Plain>
+    ],
+    Tiny: [
+      <Block.Plain blue style={{padding: '20px'}}>
+        <Loader color='white' size='tiny' />
+      </Block.Plain>
+    ]
+  }]
 }
