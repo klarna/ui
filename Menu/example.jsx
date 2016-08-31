09d61cd8849eaa4a1964d4e810bfce86b7e0c1bc
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-31 10:10:02 +0200

    Add Menu example

--- Menu/example.jsx
+++ Menu/example.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import * as Menu from '../Menu'
 import * as UncontrolledMenu from '../uncontrolled/Menu'
 import Label from '../Label'
-import { Title, Subtitle, Paragraph } from '../Text'
-import Code from '../Code'
+import * as Block from '../Block'
 
 const options = [
   { key: 'home', label: 'Home' },
@@ -16,109 +15,89 @@ const optionsWithComponents = [
   { key: 'faq', label: <div>To do <Label design='warning'>urgent</Label></div> }
 ]
 
-export default function Menus () {
-  return (
-    <div>
-      <Title.Secondary margins color='blue'>Tabs</Title.Secondary>
-      <Subtitle margins>Fluid</Subtitle>
-      <Paragraph.Primary margins>
-        Click and check your console.
-      </Paragraph.Primary>
-      <Code>
-        <Menu.Tab
-          onChange={(key) => console.log('You selected', key)}
-          tabDisplay='fluid'
-          name='tab-fluid'
-          value='home'
-          options={options} />
-      </Code>
+const wide = (example) => ({
+  example,
+  wide: true
+})
 
-      <Subtitle margins>Static</Subtitle>
-      <Paragraph.Primary margins>
-        Click and check your console.
-      </Paragraph.Primary>
-      <Code>
-        <Menu.Tab
-          onChange={(key) => console.log('You selected', key)}
-          tabDisplay='static'
-          name='tab-static'
-          value='home'
-          options={options} />
-      </Code>
-
-      <Subtitle margins>White</Subtitle>
-      <Paragraph.Primary margins>
-        Click and check your console.
-      </Paragraph.Primary>
-      <Code>
-        <div style={{background: '#0074c8', padding: '20px'}}>
-          <Menu.Tab
-            onChange={(key) => console.log('You selected', key)}
-            tabDisplay='static'
-            name='tab-static-white'
-            value='home'
-            white
-            options={options} />
-        </div>
-      </Code>
-
-      <Subtitle margins>Options with components</Subtitle>
-      <Paragraph.Primary margins>
-        Options also support components as labels.
-      </Paragraph.Primary>
-      <Code>
+export default {
+  title: 'Menu',
+  icon: 'Navigation',
+  variations: [{
+    title: 'Tabs',
+    Fluid: wide([
+      <Menu.Tab
+        onChange={(key) => console.log('You selected', key)}
+        tabDisplay='fluid'
+        name='tab-fluid'
+        value='home'
+        options={options}
+      />
+    ]),
+    Static: wide([
+      <Menu.Tab
+        onChange={(key) => console.log('You selected', key)}
+        tabDisplay='static'
+        name='tab-static'
+        value='home'
+        options={options} />
+    ]),
+    Uncontrolled: wide([
+      <UncontrolledMenu.Tab
+        name='uncontrolled-tab'
+        tabDisplay='fluid'
+        value='home'
+        options={options}
+      />
+    ]),
+    White: wide([
+      <Block.Plain blue>
         <Menu.Tab
           onChange={(key) => console.log('You selected', key)}
           tabDisplay='static'
-          name='options-with-components'
+          name='tab-static-white'
           value='home'
-          options={optionsWithComponents} />
-      </Code>
-
-      <Subtitle margins>Uncontrolled</Subtitle>
-      <Code>
-        <UncontrolledMenu.Tab
-          name='uncontrolled-tab'
-          tabDisplay='fluid'
-          value='home'
-          options={options} />
-      </Code>
-
-      <Title.Secondary margins color='blue'>Segmented</Title.Secondary>
-      <Subtitle margins>Fluid</Subtitle>
-      <Paragraph.Primary margins>
-        Click and check your console.
-      </Paragraph.Primary>
-      <Code>
-        <Menu.Segmented
-          onChange={(key) => console.log('You selected', key)}
-          name='segmented-fluid'
-          value='home'
-          options={options} />
-      </Code>
-
-      <Subtitle margins>Static</Subtitle>
-      <Paragraph.Primary margins>
-        Click and check your console.
-      </Paragraph.Primary>
-      <Code>
-        <Menu.Segmented
-          onChange={(key) => console.log('You selected', key)}
-          tabDisplay='static'
-          name='segmented-static'
-          focus='faq'
-          value='home'
-          options={options} />
-      </Code>
-
-      <Subtitle margins>Uncontrolled</Subtitle>
-      <Code>
-        <UncontrolledMenu.Segmented
-          name='uncontrolled-segmented'
-          focus='faq'
-          value='home'
-          options={options} />
-      </Code>
-    </div>
-  )
+          white
+          options={options}
+        />
+      </Block.Plain>
+    ]),
+    'With components as labels': wide([
+      <Menu.Tab
+        onChange={(key) => console.log('You selected', key)}
+        tabDisplay='static'
+        name='options-with-components'
+        value='home'
+        options={optionsWithComponents}
+      />
+    ])
+  }, {
+    title: 'Segmented',
+    Fluid: wide([
+      <Menu.Segmented
+        onChange={(key) => console.log('You selected', key)}
+        name='segmented-fluid'
+        value='home'
+        options={options}
+      />
+    ]),
+    Static: wide([
+      <Menu.Segmented
+        onChange={(key) => console.log('You selected', key)}
+        tabDisplay='static'
+        name='segmented-static'
+        focus='faq'
+        value='home'
+        options={options}
+      />
+    ]),
+    Uncontrolled: wide([
+      <UncontrolledMenu.Segmented
+        name='uncontrolled-segmented'
+        focus='faq'
+        value='home'
+        options={options}
+      />
+    ])
+  }]
 }
