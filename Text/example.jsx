841490e38644ea432c51761383ad35b0687f2b27
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-31 16:49:38 +0200

    Add multiple colors to all text styles

--- Text/example.jsx
+++ Text/example.jsx
@@ -12,6 +12,10 @@ export default {
       example: [
         <Title.Primary>Primary Title</Title.Primary>,
         <Title.Primary color='blue'>Primary Title in blue</Title.Primary>,
+        <Title.Primary color='gray'>Primary Title in gray</Title.Primary>,
+        <Title.Primary color='error'>Primary Title in error</Title.Primary>,
+        <Title.Primary color='success'>Primary Title in success</Title.Primary>,
+        <Title.Primary color='warning'>Primary Title in warning</Title.Primary>,
         <Block.Plain blue>
           <Title.Primary color='white'>Primary Title in white</Title.Primary>
         </Block.Plain>
@@ -47,6 +51,10 @@ export default {
       example: [
         <Title.Secondary>Secondary Title</Title.Secondary>,
         <Title.Secondary color='blue'>Secondary Title in blue</Title.Secondary>,
+        <Title.Secondary color='gray'>Secondary Title in gray</Title.Secondary>,
+        <Title.Secondary color='error'>Secondary Title in error</Title.Secondary>,
+        <Title.Secondary color='success'>Secondary Title in success</Title.Secondary>,
+        <Title.Secondary color='warning'>Secondary Title in warning</Title.Secondary>,
         <Block.Plain blue>
           <Title.Secondary color='white'>Secondary Title in white</Title.Secondary>
         </Block.Plain>
@@ -68,7 +76,11 @@ export default {
       example: [
         <Subtitle>Subtitle</Subtitle>,
         <Subtitle color='blue'>Subtitle in blue</Subtitle>,
-        <Block.Plain blue>
+        <Subtitle color='gray'>Subtitle in gray</Subtitle>,
+        <Subtitle color='error'>Subtitle in error</Subtitle>,
+        <Subtitle color='success'>Subtitle in success</Subtitle>,
+        <Subtitle color='warning'>Subtitle in warning</Subtitle>,
+        <Block.Plain blue style={{padding: '10px'}}>
           <Subtitle color='white'>Subtitle in white</Subtitle>
         </Block.Plain>
       ],
@@ -98,6 +110,18 @@ export default {
           Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
         </Paragraph.Primary>,
         <Paragraph.Primary color='blue'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Primary>,
+        <Paragraph.Primary color='gray'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Primary>,
+        <Paragraph.Primary color='error'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Primary>,
+        <Paragraph.Primary color='success'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Primary>,
+        <Paragraph.Primary color='warning'>
           Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
         </Paragraph.Primary>,
         <Block.Plain blue>
@@ -138,6 +162,18 @@ export default {
         <Paragraph.Secondary color='blue'>
           Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
         </Paragraph.Secondary>,
+        <Paragraph.Secondary color='gray'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Secondary>,
+        <Paragraph.Secondary color='error'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Secondary>,
+        <Paragraph.Secondary color='success'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Secondary>,
+        <Paragraph.Secondary color='warning'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Secondary>,
         <Block.Plain blue>
           <Paragraph.Secondary color='white'>
             Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
@@ -174,6 +210,18 @@ export default {
           Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
         </Paragraph.Legal>,
         <Paragraph.Legal color='blue'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Legal>,
+        <Paragraph.Legal color='gray'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Legal>,
+        <Paragraph.Legal color='error'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Legal>,
+        <Paragraph.Legal color='success'>
+          Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
+        </Paragraph.Legal>,
+        <Paragraph.Legal color='warning'>
           Proin scelerisque, felis sit amet maximus mattis, turpis ipsum interdum sem, a lacinia magna sem vel diam. Duis lacinia orci et velit faucibus, at varius mauris consectetur.
         </Paragraph.Legal>,
         <Block.Plain blue>
@@ -252,6 +300,10 @@ export default {
         <Amount>1500€</Amount>,
         <Amount>$1500</Amount>,
         <Amount color='blue'>1500₪</Amount>,
+        <Amount color='gray'>£1500</Amount>,
+        <Amount color='error'>¥1500</Amount>,
+        <Amount color='success'>1500kr</Amount>,
+        <Amount color='warning'>1500CHF</Amount>,
         <Block.Plain blue>
           <Amount color='white'>1500€</Amount>
         </Block.Plain>
