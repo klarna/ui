ffa7aba00a19d03d9394e64f924ded1a98bc6f55
Joakim Löfgren <joakim.lofgren@klarna.com>
2016-08-31 15:56:13 +0200

    Switch react to jsx string lib

--- Code/asCode.js
+++ Code/asCode.js
@@ -1,9 +1,15 @@
 import flip from 'ramda/src/flip'
 import map from 'ramda/src/map'
-import asString from 'react-to-jsx'
+import asString from 'react-element-to-jsx-string'
 import { Children } from 'react'
 
+const opts = {
+  showDefaultProps: false,
+  showFunctions: true,
+  useBooleanShorthandSyntax: true
+}
+
 const asCode = (children) =>
-  map(flip(asString)({ indent: '  ' }), Children.toArray(children)).join('')
+  map(flip(asString)(opts), Children.toArray(children)).join('')
 
 export default asCode
