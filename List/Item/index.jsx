b34c2ba69412ad853c18bcf0124b790f2052d2f3
Fernando Via Canel <fernando.canel@klarna.com>
2016-09-26 11:23:53 +0200

    Update bullets for unordered and ordered lists

--- List/Item/index.jsx
+++ List/Item/index.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import classNamesBind from 'classnames/bind'
 import defaultStyles from '../styles.scss'
 
-const baseClass = 'list--item'
+const baseClass = 'list__item'
+
+const classes = {
+  bulletUnordered: `${baseClass}__bullet--unordered`
+}
 
 export default function Item ({className, children, color, styles, ...props}) {
   const classNames = classNamesBind.bind({...defaultStyles, ...styles})
 
   return (
     <li className={classNames(baseClass, color, className)} {...props}>
+      <div className={classNames(classes.bulletUnordered)} />
       {children}
     </li>
   )
