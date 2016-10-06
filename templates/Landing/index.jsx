c09f9c12badc75d5ec2408b66c0af56a932dc2aa
Fernando Via Canel <fernando.canel@klarna.com>
2016-10-06 14:15:19 +0200

    Fix SASS lint

--- templates/Landing/index.jsx
+++ templates/Landing/index.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
-import classNamesBind from 'classnames/bind'
 import Centered from '../chromes/Centered'
-import defaultStyles from './styles.scss'
 
-const baseClass = 'landing'
-
-// const classes = {
-//   title: `${baseClass}__title`,
-//   paragraphPrimary: `${baseClass}__paragraph--primary`,
-//   buttonAccept: `${baseClass}__button--accept`,
-//   legal: `${baseClass}__paragraph--legal`
-// }
-
-export default function Landing ({
-  ...props
-}) {
+export default function Landing ({...props}) {
   return <Centered {...props} />
 }
