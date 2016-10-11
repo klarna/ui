c239935fadc7933a53df7e24d02a34e54c2aebb7
Fernando Via Canel <fernando.canel@klarna.com>
2016-10-11 15:43:38 +0200

    Remove unused themeables/texts files

--- themeable/texts/Subtitle.jsx
+++
@@ -1,9 +0,0 @@
-import Subtitle from '../../texts/Subtitle'
-import themeable from '../../../decorators/themeable'
-
-export default themeable(Subtitle, (customizations, props) => ({
-  style: {
-    ...props.style,
-    color: customizations.color_header
-  }
-}))
