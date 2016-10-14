2b61ebe46caf2ccd52f880fef35c0c8193154c1a
Fernando Via Canel <fernando.canel@klarna.com>
2016-10-14 17:09:30 +0200

    Fix API changes

--- Showroom/examples/compositions/UncontrolledForm.jsx
+++ Showroom/examples/compositions/UncontrolledForm.jsx
@@ -15,119 +15,109 @@ export default {
   variations: [
     {
       title: 'Address form',
-      require: `import Fieldset from '@klarna/ui/Fieldset'
-  import UncontrolledField from '@klarna/ui/uncontrolled/Field'`,
       type: SHOWCASE,
 
-      example: [
-        <form>
-          <Fieldset margins>
-            <UncontrolledMenu.Tab
-              name='consumer-type'
-              options={[
-                {key: 'person', label: 'Person'},
-                {key: 'company', label: 'Company'}
-              ]}
-              value='person'
-            />
-          </Fieldset>
+      example: <form>
+        <Fieldset margins>
+          <UncontrolledMenu.Tab
+            name='consumer-type'
+            options={[
+              {key: 'person', label: 'Person'},
+              {key: 'company', label: 'Company'}
+            ]}
+            value='person'
+          />
+        </Fieldset>
 
-          <Fieldset margins>
-            <UncontrolledSelector
-              name='shipping-option'
-              data={[
-                {key: 'express', label: 'Express shipping'},
-                {key: 'overnight', label: 'Overnight shipping'}
-              ]}
-            />
-          </Fieldset>
+        <Fieldset margins>
+          <UncontrolledSelector
+            name='shipping-option'
+            data={[
+              {key: 'express', label: 'Express shipping'},
+              {key: 'overnight', label: 'Overnight shipping'}
+            ]}
+          />
+        </Fieldset>
 
-          <Fieldset margins>
-            <UncontrolledField
-              label='First name'
-              name='firstname'
-              top left size='1/2'
-            />
-            <UncontrolledField
-              label='Last name'
-              name='lastname'
-              top right size='1/2'
-            />
-            <UncontrolledField
-              label='Street address'
-              name='streetaddress'
-              square size='3/4'
-            />
-            <UncontrolledDropdown
-              label='Continent'
-              name='continent'
-              center size='1/4'
-              options={[
-                {key: '', disabled: true, hidden: true},
-                {key: 'africa', label: 'Africa'},
-                {key: 'america', label: 'America'},
-                {key: 'antarctica', label: 'Antarctica'},
-                {key: 'asia', label: 'Asia'},
-                {key: 'europe', label: 'Europe'},
-                {key: 'oceania', label: 'Oceania'}
-              ]}
-            />
-            <UncontrolledField
-              label='Email'
-              name='email'
-              bottom left size='1/2'
-            />
-            <UncontrolledField
-              label='Phone number'
-              name='phonenumber'
-              bottom right size='1/2'
-            />
-          </Fieldset>
+        <Fieldset margins>
+          <UncontrolledField
+            label='First name'
+            name='firstname'
+            top left size='1/2'
+          />
+          <UncontrolledField
+            label='Last name'
+            name='lastname'
+            top right size='1/2'
+          />
+          <UncontrolledField
+            label='Street address'
+            name='streetaddress'
+            square size='3/4'
+          />
+          <UncontrolledDropdown
+            label='Continent'
+            name='continent'
+            center size='1/4'
+            options={[
+              {key: '', disabled: true, hidden: true},
+              {key: 'africa', label: 'Africa'},
+              {key: 'america', label: 'America'},
+              {key: 'antarctica', label: 'Antarctica'},
+              {key: 'asia', label: 'Asia'},
+              {key: 'europe', label: 'Europe'},
+              {key: 'oceania', label: 'Oceania'}
+            ]}
+          />
+          <UncontrolledField
+            label='Email'
+            name='email'
+            bottom left size='1/2'
+          />
+          <UncontrolledField
+            label='Phone number'
+            name='phonenumber'
+            bottom right size='1/2'
+          />
+        </Fieldset>
 
-          <Fieldset margins>
-            <UncontrolledSwitch.Checkbox
-              name='separate-shipping-address'>
-              Separate shipping address
-            </UncontrolledSwitch.Checkbox>
-          </Fieldset>
+        <Fieldset margins>
+          <UncontrolledSwitch.Checkbox
+            name='separate-shipping-address'>
+            Separate shipping address
+          </UncontrolledSwitch.Checkbox>
+        </Fieldset>
 
-          <Fieldset margins>
-            <Subtitle>How many installments do you want to have?</Subtitle>
-            <UncontrolledInstallments
-              name='installments'
-              options={[
-                {
-                  connector: ' for ',
-                  info: '3 months',
-                  key: 'installments_3',
-                  value: '$70.17/mo.'
-                },
-                {
-                  connector: ' for ',
-                  info: '6 months',
-                  key: 'installments_6',
-                  value: '$64.17/mo.'
-                },
-                {
-                  connector: ' for ',
-                  info: '12 months',
-                  key: 'installments_12',
-                  value: '$32.09/mo.'
-                }
-              ]}
-              value='installments_3'
-            />
-          </Fieldset>
+        <Fieldset margins>
+          <Subtitle>How many installments do you want to have?</Subtitle>
+          <UncontrolledInstallments
+            name='installments'
+            options={[
+              {
+                content: '3 months for $70.17/mo.',
+                key: 'installments_3'
+              },
+              {
+                content: '6 months for $64.17/mo.',
+                key: 'installments_6'
+              },
+              {
+                content: '12 months for $32.09/mo.',
+                key: 'installments_12'
+              }
+            ]}
+            value='installments_3'
+          />
+        </Fieldset>
 
-          <Button.Primary type='submit' style={{marginRight: '20px'}}>
-            Submit
-          </Button.Primary>
+        <Button.Primary type='submit' style={{marginRight: '20px'}}>
+          Submit
+        </Button.Primary>
 
-          <Button.Secondary>
-            Cancel
-          </Button.Secondary>
-        </form>
-      ]
+        <Button.Secondary>
+          Cancel
+        </Button.Secondary>
+      </form>
     }
   ]
 }
