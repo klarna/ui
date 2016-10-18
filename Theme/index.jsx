11ef1c4e8b7e06b6ec6d7abc17b8535ef1b7d8e5
Fernando Via Canel <fernando.canel@klarna.com>
2016-10-18 16:24:16 +0200

    Make all props in Theme required

--- Theme/index.jsx
+++ Theme/index.jsx
@@ -3,18 +3,18 @@ import { getContextualizer } from 'react-context-props'
 
 const Theme = getContextualizer({
   customizations: PropTypes.shape({
-    color_button: PropTypes.string,
-    color_button_text: PropTypes.string,
-    color_checkbox: PropTypes.string,
-    color_checkbox_checkmark: PropTypes.string,
-    color_text: PropTypes.string,
-    color_header: PropTypes.string,
-    color_link: PropTypes.string,
-    color_border: PropTypes.string,
-    color_border_selected: PropTypes.string,
-    color_details: PropTypes.string,
-    color_text_secondary: PropTypes.string,
-    radius_border: PropTypes.string
+    color_button: PropTypes.string.isRequired,
+    color_button_text: PropTypes.string.isRequired,
+    color_checkbox: PropTypes.string.isRequired,
+    color_checkbox_checkmark: PropTypes.string.isRequired,
+    color_text: PropTypes.string.isRequired,
+    color_header: PropTypes.string.isRequired,
+    color_link: PropTypes.string.isRequired,
+    color_border: PropTypes.string.isRequired,
+    color_border_selected: PropTypes.string.isRequired,
+    color_details: PropTypes.string.isRequired,
+    color_text_secondary: PropTypes.string.isRequired,
+    radius_border: PropTypes.string.isRequired
   })
 })
 
