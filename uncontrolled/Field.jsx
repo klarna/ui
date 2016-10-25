b37d3a326e986b3fc07ce70f62445ac574ed6fd9
Tommy Brunn <tommy.brunn@klarna.com>
2016-10-25 10:21:38 +0200

    Generate unique name for uncontrolled Field

--- uncontrolled/Field.jsx
+++ uncontrolled/Field.jsx
@@ -1,9 +1,10 @@
 import compose from '../lib/compose'
 import statefulValue from '../lib/decorators/statefulValue'
 import statefulFocus from '../lib/decorators/statefulFocus'
+import name from '../lib/decorators/name'
 import Field from '../Field'
 
-const UncontrolledField = compose(statefulFocus, statefulValue)(Field)
+const UncontrolledField = compose(name, statefulFocus, statefulValue)(Field)
 
 UncontrolledField.displayName = 'UncontrolledField'
 
