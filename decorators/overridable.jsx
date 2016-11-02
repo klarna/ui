c82366828e3fc5170546a8a095da40b1d5981986
Pierre Wahlgren <pierre.wahlgren@klarna.com>
2016-11-02 15:46:13 +0100

    Add "overridable" decorator

---
+++ decorators/overridable.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { withPropsFromContext } from 'react-context-props'
+
+const overridable = (styles = {}, designName) => (Target) => {
+  const OverridableComponent = React.createClass({
+    displayName: `overridable(${Target.displayName || Target.name})`,
+    designName: designName || Target.displayName || Target.name,
+    getDefaultProps () {
+      return {
+        design: {},
+        styles: {}
+      }
+    },
+    componentWillMount () {
+      this.styles = { ...styles, ...this.props.styles }
+      this.getAndSetOverride()
+    },
+    componentWillUpdate () {
+      this.getAndSetOverride()
+    },
+    getAndSetOverride () {
+      if (this.props.design.getOverrideFor) {
+        const override = this.props.design.getOverrideFor(
+          Object.assign(Target, { designName: this.designName })
+        )
+        this.Component = override.Component
+        this.styles = { ...this.styles, ...override.css }
+      } else {
+        this.Component = Target
+      }
+    },
+    render () {
+      const props = { ...this.props, styles: this.styles }
+      return <this.Component { ...props } />
+    }
+  })
+
+  return withPropsFromContext(
+    OverridableComponent,
+    ['design']
+  )
+}
+
+export default overridable
