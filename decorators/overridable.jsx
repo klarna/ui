824dee490794181875f72215a731f7356df37652
Pierre Wahlgren <pierre.wahlgren@klarna.com>
2016-11-02 17:19:10 +0100

    Use ES6 class instead of `React.createClass`

--- decorators/overridable.jsx
+++ decorators/overridable.jsx
@@ -2,39 +2,42 @@ import React from 'react'
 import { withPropsFromContext } from 'react-context-props'
 
 const overridable = (styles = {}, designName) => (Target) => {
-  const OverridableComponent = React.createClass({
-    displayName: `overridable(${Target.displayName || Target.name})`,
-    designName: designName || Target.displayName || Target.name,
-    getDefaultProps () {
-      return {
-        design: {},
-        styles: {}
-      }
-    },
+  class OverridableComponent extends React.Component {
+    constructor (props) {
+      super(props)
+
+      this.designName = designName ||  Target.displayName || Target.name
+      this.Component = Target
+    }
     componentWillMount () {
       this.styles = { ...styles, ...this.props.styles }
       this.getAndSetOverride()
-    },
+    }
     componentWillUpdate () {
       this.getAndSetOverride()
-    },
+    }
     getAndSetOverride () {
-      if (this.props.design.getOverrideFor) {
-        const override = this.props.design.getOverrideFor(
-          Object.assign(Target, { designName: this.designName })
-        )
-        this.Component = override.Component
-        this.styles = { ...this.styles, ...override.css }
-      } else {
-        this.Component = Target
+      if (!this.props.design.getOverrideFor) {
+        return
       }
-    },
+      const override = this.props.design.getOverrideFor(
+        Object.assign(Target, { designName: this.designName })
+      )
+      this.Component = override.Component
+      this.styles = { ...this.styles, ...override.css }
+    }
     render () {
       const { design, ...otherProps } = this.props // eslint-disable-line
       const props = { ...otherProps, styles: this.styles }
       return <this.Component { ...props } />
     }
-  })
+  }
+
+  OverridableComponent.displayName = `overridable(${Target.displayName || Target.name})`,
+  OverridableComponent.defaultProps = {
+    design: {},
+    styles: {}
+  }
 
   return withPropsFromContext(
     OverridableComponent,
