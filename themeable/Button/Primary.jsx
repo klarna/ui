d3e8dfd5c7f41729b4c3888d25977a8858426c63
Fernando Via Canel <fernando.canel@klarna.com>
2016-11-08 17:22:49 +0100

    Remove all remaining themeables

--- themeable/Button/Primary.jsx
+++
@@ -1,9 +0,0 @@
-import Primary from '../../Button/Primary'
-import deprecated from '../../decorators/deprecated'
-
-export default deprecated(
-  Primary,
-  'ThemeableButton.Primary',
-  'Button.Primary',
-  'https://github.com/klarna/ui/releases/tag/v1.5.0'
-)
