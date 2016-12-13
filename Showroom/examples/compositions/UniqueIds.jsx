f89a28105f490e3a7f9bbef987ecf44643f7ba97
Fernando Via Canel <fernando.canel@klarna.com>
2016-12-13 16:50:41 +0100

    Add Radio unique id example

--- Showroom/examples/compositions/UniqueIds.jsx
+++ Showroom/examples/compositions/UniqueIds.jsx
@@ -20,6 +20,7 @@ import * as Title from '../../../Title'
 import * as Paragraph from '../../../Paragraph'
 import * as Preview from '../../../Preview'
 import ProgressBar from '../../../ProgressBar'
+import Radio from '../../../Radio'
 import Subtitle from '../../../Subtitle'
 import {LIVE, MANUAL} from '../../variationTypes'
 
@@ -510,6 +511,76 @@ import * as Button from '@klarna/ui/Button'`,
         ProgressBar: <ProgressBar
           id='progress-bar'
           percentage={50}
+        />
+      }
+    },
+
+    {
+      title: 'Radio',
+      require: 'import Radio from \'@klarna/ui/Radio\'',
+      type: LIVE,
+
+      examples: {
+        Radio: <Radio
+          onChange={(key) => console.log(key)}
+          name='radio-regular'
+          options={[
+            {
+              key: 'lorem',
+              label: 'Lorem',
+              description: 'Lorem Ipsum is simply dummy.',
+              aside: <svg width='32px' height='22px' viewBox='0 0 32 22'>
+                <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
+                  <rect fill='#0074C8' x='0' y='0' width='32' height='22' rx='2' />
+                  <polygon fill='#E5E5E6' points='23 7 27 15 19 15' />
+                </g>
+              </svg>,
+              content: <Paragraph.Secondary condensed>
+                Offal man braid XOXO DIY, pok pok tbh poke post-ironic neutra try-hard small batch.
+              </Paragraph.Secondary>,
+              leftPad: true
+            },
+
+            {
+              key: 'sit',
+              label: 'Sit',
+              description: 'Amet et consequetur',
+              content: <div>
+                <Installments
+                  name='installments'
+                  onChange={(key) => console.log('You selected', key)}
+                  options={[{content: <div>Long one line text in div</div>, key: 'installments_3'}, {content: [<div key='1'>$64.17/mo.</div>, <div key='2'>array of elements</div>], key: 'installments_6'}]}
+                  value='installments_6'
+                />
+                <Checklist.Main style={{marginTop: '20px'}}>
+                  <Checklist.Item>Just one click and you’re done</Checklist.Item>
+                  <Checklist.Item>Very little hassle</Checklist.Item>
+                  <Checklist.Item>Just do it! It can be done today, so why wait for  tomorrow?</Checklist.Item>
+                </Checklist.Main>
+              </div>
+            },
+
+            {
+              key: 'ipsum',
+              label: 'Ipsum',
+              description: 'Dummy text ever since the 1500s.',
+              content: <div>
+                <Subtitle>Choose your destiny</Subtitle>
+
+                <Dropdown
+                  name='ipsum-uncontrolled-dropdown'
+                  options={[
+                    {key: 'coffee', label: 'Coffee'},
+                    {key: 'chai', label: 'Chai'},
+                    {key: 'latte', label: 'Latte'}
+                  ]}
+                  label='Infusion'
+                  value='coffee'
+                />
+              </div>
+            }
+          ]}
+          defaultValue='lorem'
         />
       }
     }
