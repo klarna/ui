9fd27f946797fafda3dd7b023efcbf604ae437e1
Fernando Via Canel <fernando.canel@klarna.com>
2016-12-13 14:23:15 +0100

    Add ContextMenu unique id example

--- Showroom/examples/compositions/UniqueIds.jsx
+++ Showroom/examples/compositions/UniqueIds.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import * as Alert from '../../../Alert'
 import * as Block from '../../../Block'
 import * as Button from '../../../Button'
-import { Back } from '../../../IconButton'
+import * as Checklist from '../../../Checklist'
+import * as ContextMenu from '../../../ContextMenu'
+import {Back} from '../../../IconButton'
+import Logout from '../../../icons/Logout'
 import {LIVE} from '../../variationTypes'
 
 export default {
@@ -88,6 +91,56 @@ import { Back } from '@klarna/ui/IconButton'`,
           style={{display: 'inline-block', padding: '20px'}}>
           <Back id='iconic-button-back' />
         </Block.Plain>
+      }
+    },
+
+    {
+      title: 'Checklist',
+      require: 'import * as Checklist from \'@klarna/ui/Checklist\'',
+      type: LIVE,
+
+      examples: {
+        Checklist: <Checklist.Main id='checklist-main'>
+          <Checklist.Item id='checklist-main-item1'>
+            Just one click and you're done
+          </Checklist.Item>
+          <Checklist.Item id='checklist-main-item2'>
+            Very little hassle
+          </Checklist.Item>
+          <Checklist.Item id='checklist-main-item3'>
+            Just do it! It can be done today, so why wait for  tomorrow?
+          </Checklist.Item>
+        </Checklist.Main>
+      }
+    },
+
+    {
+      title: 'ContextMenu',
+      require: `import * as ContextMenu from '@klarna/ui/ContextMenu'
+import Logout from '@klarna/ui/Logout'`,
+      type: LIVE,
+      examples: {
+        ContextMenu: <ContextMenu.Main id='context-menu-main'>
+          <ContextMenu.Link id='context-menu-link1'>
+            Buttons
+          </ContextMenu.Link>
+          <ContextMenu.Link id='context-menu-link2'>
+            Icons
+          </ContextMenu.Link>
+          <ContextMenu.Link id='context-menu-link3'>
+            Labels
+          </ContextMenu.Link>
+          <ContextMenu.Item id='context-menu-link4'>
+            <form>Custom form</form>
+          </ContextMenu.Item>
+          <ContextMenu.Separator id='context-menu-separator' />
+          <ContextMenu.Link id='context-menu-link5'>
+            <ContextMenu.Icon id='context-menu-icon'>
+              <Logout />
+            </ContextMenu.Icon>
+            Logout
+          </ContextMenu.Link>
+        </ContextMenu.Main>
       }
     }
   ]
