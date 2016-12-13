74e926d7ca19577c0e1d46d55a33d7c9efdcbc32
Fernando Via Canel <fernando.canel@klarna.com>
2016-12-13 10:01:03 +0100

    Add ids in the Link

--- Preview/index.jsx
+++ Preview/index.jsx
@@ -35,7 +35,7 @@ Main.propTypes = {
   styles: PropTypes.object
 }
 
-export function Content ({ children, className, styles, ...props }) {
+export function Content ({children, className, styles, ...props }) {
   const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
 
   return (
@@ -53,8 +53,8 @@ Content.propTypes = {
   styles: PropTypes.object
 }
 
-export function Title ({ children, className, styles, ...props }) {
-  const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
+export function Title ({children, className, styles, ...props}) {
+  const classNames = classNamesBind.bind({...defaultStyles, ...styles})
 
   return (
     <h2 className={classNames(classes.title, className)} {...props}>
@@ -71,12 +71,19 @@ Title.propTypes = {
   styles: PropTypes.object
 }
 
-export function Link ({ children, className, styles, ...props }) {
-  const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
+export function Link ({children, className, id, styles, ...props}) {
+  const classNames = classNamesBind.bind({...defaultStyles, ...styles})
+  const ids = id
+    ? {
+      link: `${id}__link`
+    } : {}
 
   return (
-    <div className={classNames(classes.footer)}>
-      <a className={classNames(classes.footerLink, className)} {...props}>
+    <div className={classNames(classes.footer)} id={id}>
+      <a
+        className={classNames(classes.footerLink, className)}
+        id={ids.link}
+        {...props}>
         {children}
       </a>
     </div>
