8a41346be45303421fdb03a24db29a18e6b148c7
Paulo Ragonha <paulo@ragonha.me>
2016-12-29 15:17:32 +0100

    Change Collapsible to use CSS Transitions instead of React Motion
    This has proven to be slightly faster and has the benefit of not triggering animations at all on old browsers that would be slow with React Motion.

--- Collapsible/index.jsx
+++ Collapsible/index.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import {Motion, spring} from 'react-motion'
 import debounce from '../lib/debounce'
+import defaultStyles from './styles.scss'
 
 export default class Collapsible extends Component {
   constructor () {
@@ -49,22 +49,15 @@ export default class Collapsible extends Component {
     const {children, collapsed} = this.props
 
     return <div ref={(div) => { this.content = div }}>
-      <Motion style={{
-        height: spring(collapsed ? 0 : this.state.height),
-        opacity: spring(collapsed ? 0 : 1)
-      }}>
-        {({height, opacity}) => <div
-          style={{
-            height,
-            opacity,
-            // Overflow rule to enable content to overflow outside the collapsible
-            // once the animation is close to be complete (the last few pixels take a while
-            // to be expanded). '10' is a magic number 🎩
-            overflow: this.state.height - height > 10 ? 'hidden' : 'visible'
-          }}>
-          {children}
-        </div>}
-      </Motion>
+      <div
+        className={defaultStyles.wrapper}
+        style={{
+          height: collapsed ? 0 : this.state.height,
+          opacity: collapsed ? 0 : 1,
+          overflow: collapsed ? 'hidden' : 'visible'
+        }}>
+        {children}
+      </div>
     </div>
   }
 }
