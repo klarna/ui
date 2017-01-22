cadfd85eca82a19686d96c8456b05a9454dc0e1f
Fernando Via Canel <fernando.canel@klarna.com>
2017-01-22 10:16:58 +0200

    Will be removed from this repo anyway

--- Collapsible/example.jsx
+++ Collapsible/example.jsx
@@ -25,9 +25,11 @@ class CollapsibleExample extends Component {
         lowFPS={false}
         onStartFPSCollection={() => console.log('Start FPS collection')}
         onEndFPSCollection={() => console.log('End FPS collection')}>
-        <Title.Primary>Collapsible demonstration</Title.Primary>
+        <Title.Primary>
+          Collapsible demonstration
+        </Title.Primary>
         <Paragraph.Primary>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit
         </Paragraph.Primary>
       </Collapsible>
     </div>
