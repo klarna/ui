47799e777f789aaba56de82287f7b7ed480c5361
Fernando Via Canel <fernando.canel@klarna.com>
2017-02-02 14:03:35 +0100

    Make the Backdrop minimal

--- Backdrop/index.jsx
+++ Backdrop/index.jsx
@@ -4,73 +4,8 @@ import defaultStyles from './styles.scss'
 
 const baseClass = 'backdrop'
 
-const classes = {
-  smoke: `${baseClass}__smoke`
-}
+export default function Backdrop ({className, styles, ...props}) {
+  const classNames = classNamesBind.bind({...defaultStyles, ...styles})
 
-export default class Backdrop extends Component {
-  constructor () {
-    super()
-
-    this.state = {
-      top: undefined
-    }
-  }
-
-  componentWillMount () {
-    if (this.props.pinBody) {
-      pinBody(this)
-    }
-  }
-
-  componentWillUnmount () {
-    unPinBody(this.state.top)
-  }
-
-  componentDidUpdate () {
-    if (this.props.pinBody) {
-      pinBody(this)
-    }
-  }
-
-  render () {
-    const {
-      children,
-      pinBody,
-      styles,
-      ...props
-    } = this.props
-    const classNames = classNamesBind.bind({...defaultStyles, ...styles})
-
-    return <div
-      className={classNames(baseClass)}
-      style={pinBody ? {top: this.state.top} : undefined}
-      {...props}>
-      <div className={classNames(classes.smoke)}>
-        {children}
-      </div>
-    </div>
-  }
-}
-
-const pinBody = (component) => {
-  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
-
-  document.body.style.height = '100%'
-  document.body.style.position = 'fixed'
-  document.body.style.top = `-${scrollTop}px`
-  document.body.style.width = '100%'
-
-  if (component.state.top !== scrollTop) {
-    component.setState({top: scrollTop})
-  }
-}
-
-const unPinBody = (top) => {
-  document.body.style.height = 'auto'
-  document.body.style.position = 'static'
-  document.body.style.top = '0'
-  document.body.style.width = 'auto'
-  console.log('the top', top)
-  document.documentElement.scrollTop = document.body.scrollTop = top
+  return <div className={classNames(baseClass, className)} {...props} />
 }
