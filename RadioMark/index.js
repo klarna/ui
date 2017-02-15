8362e91f278e08c6954582ad55e74332a589bdf8
Fernando Via Canel <fernando.canel@klarna.com>
2017-02-15 16:59:03 +0100

    Support full style overrides in RadioMark

--- RadioMark/index.js
+++ RadioMark/index.js
@@ -2,18 +2,29 @@ import React, { PropTypes } from 'react'
 import { Motion, spring } from 'react-motion'
 import compose from 'ramda/src/compose'
 import {themeable} from '@klarna/higher-order-components'
+import deepMerge from 'deepmerge'
 import defaultStyles from './styles'
 
-function RadioMark ({ customize, checked, disabled, onClick, lowFPS }) {
+function RadioMark ({
+  customize,
+  checked,
+  disabled,
+  onClick,
+  lowFPS,
+  styles,
+  ...props
+}) {
+  const finalStyles = deepMerge(defaultStyles, styles)
   const scale = checked
-    ? defaultStyles.checked.animation.scale
-    : defaultStyles.base.animation.scale
+    ? finalStyles.checked.animation.scale
+    : finalStyles.base.animation.scale
 
   return (
     <svg
-      width={defaultStyles.base.main.width}
-      height={defaultStyles.base.main.height}
-      onClick={onClick}>
+      width={finalStyles.base.main.width}
+      height={finalStyles.base.main.height}
+      onClick={onClick}
+      {...props}>
       <Motion
         style={{
           scale: lowFPS
@@ -22,18 +33,18 @@ function RadioMark ({ customize, checked, disabled, onClick, lowFPS }) {
         }}>
         {({ scale }) => (
           <g>
-            <circle {...defaultStyles.base.borderCircle} />
+            <circle {...finalStyles.base.borderCircle} />
             <circle
-              {...defaultStyles.base.backgroundCircle}
+              {...finalStyles.base.backgroundCircle}
               {...(customize ? {fill: customize.backgroundColor} : {})}
-              {...(disabled ? defaultStyles.disabled.backgroundCircle : {})}
+              {...(disabled ? finalStyles.disabled.backgroundCircle : {})}
               style={{ opacity: 1 - (scale - 1) / 2 }}
             />
             {/* tranformOrigin using matrices: http://stackoverflow.com/questions/6711610/how-to-set-transform-origin-in-svg#6714140 */}
-            <g transform={defaultStyles.base.animation.transform(scale)}>
-              <circle {...defaultStyles.base.whiteCircle} />
+            <g transform={finalStyles.base.animation.transform(scale)}>
+              <circle {...finalStyles.base.whiteCircle} />
               <circle
-                {...defaultStyles.base.bulletCircle}
+                {...finalStyles.base.bulletCircle}
                 {...(customize ? {fill: customize.bulletColor} : {})}
                 opacity={1 - (scale - 1) / 2}
               />
@@ -53,7 +64,20 @@ RadioMark.propTypes = {
   }),
   disabled: PropTypes.bool,
   lowFPS: PropTypes.bool,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  styles: PropTypes.shape({
+    base: PropTypes.object,
+    disabled: PropTypes.object,
+    checked: PropTypes.object
+  })
+}
+
+RadioMark.defaultProps = {
+  styles: {
+    base: {},
+    disabled: {},
+    checked: {}
+  }
 }
 
 export default compose(
