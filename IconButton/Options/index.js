b70a8afef9080805d4e478a9b50fdcae8347be73
Fernando Via Canel <fernando.canel@klarna.com>
2017-02-20 16:46:04 +0100

    Options IconButton updated to inline styles

--- IconButton/Options/index.js
+++ IconButton/Options/index.js
@@ -1,22 +1,20 @@
 import React, { PropTypes } from 'react'
-import classNamesBind from 'classnames/bind'
-import defaultStyles from '../styles.scss'
+import defaultStyles from '../styles'
 import withDisplayName from '../withDisplayName'
+import {
+  withHoverProps,
+  withMouseDownProps,
+  withTouchProps,
+  overridable,
+  themeable
+} from '@klarna/higher-order-components'
 
 import compose from 'ramda/src/compose'
-import {overridable, themeable} from '@klarna/higher-order-components'
-
-const classes = {
-  bgWrapper: 'bg-wrapper',
-  iconButton: 'icon-button',
-  fill: 'illustration__fill',
-  label: 'illustration__label',
-  stroke: 'illustration__stroke'
-}
 
 const Options = ({
-  className,
+  active,
   color,
+  hover,
   id,
   label,
   left,
@@ -25,42 +23,54 @@ const Options = ({
   topRight,
   ...props
 }) => {
-  const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
   const ids = id
     ? {
       illustration: `${id}__illustration`,
-      label: `${id}__label`
+      label: `${id}__label`,
+      wrapper: `${id}__wrapper`
     } : {}
 
+  const colorMode = color == null ? 'base' : color
+
   return <div
-    className={classNames(
-      classes.iconButton,
-      {
-        'top-right': topRight,
-        'top-left': topLeft
-      },
-      className
-    )}
     id={id}
+    style={{
+      // TODO: Use `mobile` variation as well (styles.js:156)
+      ...defaultStyles.base.main,
+      ...(topRight ? defaultStyles.topRight.main : {}),
+      ...(topLeft ? defaultStyles.topLeft.main : {}),
+    }}
     {...props}>
-    <div className={classNames(classes.bgWrapper, color)}>
+    <div
+      id={ids.wrapper}
+      style={{
+        ...defaultStyles.base.wrapper,
+        ...(active ? defaultStyles.active[colorMode].wrapper : {})
+      }}>
       <svg
-        className={classNames('illustration', 'button', color)}
         id={ids.illustration}
         viewBox='0 0 25 25'
         height='20px'
         width='20px'>
         {[7, 13, 19].map((y) =>
           <circle
-            className={classNames(classes.fill)}
             key={y} cx='12' cy={y} r='2'
+            style={{
+              ...defaultStyles[colorMode].fill,
+              ...(hover ? defaultStyles.hover[colorMode].fill : {})
+            }}
           />
         )}
       </svg>
 
       <span
-        className={classNames(classes.label, { left }, color)}
-        id={ids.label}>
+        id={ids.label}
+        style={{
+          ...defaultStyles.base.label,
+          ...(left ? defaultStyles.left.label : {}),
+          ...defaultStyles[colorMode].label,
+          ...(hover ? defaultStyles.hover[colorMode].label : {})
+        }}>
         {label}
       </span>
     </div>
@@ -68,8 +78,9 @@ const Options = ({
 }
 
 Options.propTypes = {
-  className: PropTypes.string,
+  active: PropTypes.bool,
   color: PropTypes.oneOf(['gray', 'inverse', 'blue']),
+  hover: PropTypes.bool,
   id: PropTypes.string,
   styles: PropTypes.object
 }
@@ -77,5 +88,8 @@ Options.propTypes = {
 export default compose(
   themeable(() => ({color: 'gray'})),
   overridable(defaultStyles),
+  withHoverProps({hover: true}),
+  withMouseDownProps({active: true}),
+  withTouchProps({active: true}),
   withDisplayName('Options')
 )(Options)
