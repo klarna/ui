9c01f6c2b7813ecd2d95a5dcb15d1dd08d4b40fa
Fernando Via Canel <fernando.canel@klarna.com>
2017-02-27 16:49:42 +0100

    Add html2KlarnaUI

---
+++ html2KlarnaUI/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import html2react from 'html2react'
+import Strong from '../Strong'
+import * as Paragraph from '../Paragraph'
+import * as Title from '../Title'
+
+export default html2react({
+  h1: (props) => <Title.Primary margins {...props} />,
+  p: (props) => <Paragraph.Primary margins {...props} />,
+  strong: Strong,
+})
