a47873382da6186ed62f6dce8b2d7ce1e835a861
Fernando Via Canel <fernando.canel@klarna.com>
2017-03-02 13:52:49 +0100

    Update the rest of the Informative examples

--- templates/examples/Informative.jsx
+++ templates/examples/Informative.jsx
@@ -28,7 +28,6 @@ function LandingExample ({
   withPrimaryButton,
   multipleParagraphs
 }) {
-
   const content = <Wrapper
     layout={desktop
       ? 'desktop'
@@ -201,6 +200,266 @@ ExplanationExample.defaultProps = {
   narrow: false
 }
 
+function XStepExplanationExample ({
+  desktop,
+  narrow,
+  highBrandVolume
+}) {
+  const content = <Wrapper
+    layout={desktop
+      ? 'desktop'
+      : (narrow
+        ? 'narrow'
+        : 'mobile'
+      )}
+    onBack={() => console.log('welcome back')}
+    onClose={() => console.log('that was close')}>
+    <XStepExplanation
+      id='x-step-explanation'
+      title='This is the main feature headline'
+      accept='Continue'
+      bullets={[
+        {
+          icon: <svg
+            width='60px'
+            height='60px'
+            viewBox='0 0 60 60'>
+            <circle
+              stroke='#979797'
+              fill='#D8D8D8'
+              cx='30'
+              cy='30'
+              r='28'
+            />
+          </svg>,
+          content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
+        },
+        {
+          icon: <svg
+            width='60px'
+            height='60px'
+            viewBox='0 0 60 60'>
+            <polygon
+              stroke='#979797'
+              fill='#D8D8D8'
+              points='30 46.5 12.95 55.46 16.20 36.48 2.41 23.03 21.47 20.26 30 3 38.52 20.26 57.58 23.03 43.79 36.48 47.04 55.46'
+            />
+          </svg>,
+          content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
+        },
+        {
+          icon: <svg
+            width='60px'
+            height='60px'
+            viewBox='0 0 60 60'>
+            <polygon
+              stroke='#979797'
+              fill='#D8D8D8'
+              points='30 4 58 54 2 54'
+            />
+          </svg>,
+          content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
+        }
+      ]}
+      brandVolume={highBrandVolume ? 'high' : undefined}
+      onAccept={() => console.log('Continue')}
+    />
+  </Wrapper>
+
+  return <div
+    style={{
+      marginRight: grid(4),
+      width: `calc(80% - ${grid(4)})`
+    }}>
+    <Canvas
+      dimensions={desktop
+        ? undefined
+        : (narrow
+          ? [280, 500]
+          : [400, 600]
+        )}>
+      {content}
+    </Canvas>
+
+    <Code.Block standalone style={{marginTop: 20}}>
+      {asCode(content)}
+    </Code.Block>
+  </div>
+}
+
+XStepExplanationExample.options = [
+  {
+    label: 'Desktop',
+    name: 'desktop',
+    value: false
+  },
+  {
+    label: 'Narrow',
+    name: 'narrow',
+    value: false
+  },
+  {
+    label: 'High brand volume',
+    name: 'highBrandVolume',
+    value: false
+  }
+]
+
+XStepExplanationExample.defaultProps = {
+  desktop: false,
+  narrow: false,
+  highBrandVolume: false
+}
+
+function ConfirmDataExample ({
+  desktop,
+  narrow,
+  highBrandVolume
+}) {
+  const content = <Wrapper
+    layout={desktop
+      ? 'desktop'
+      : (narrow
+        ? 'narrow'
+        : 'mobile'
+      )}
+    onBack={() => console.log('welcome back')}
+    onClose={() => console.log('that was close')}>
+    <ConfirmData
+      id='confirm-data'
+      title='Are these your favorite flavors?'
+      summary='Street art tattooed live-edge, kitsch four loko hashtag paleo banh mi art party. Viral flexitarian paleo, stumptown dreamcatcher ennui pitchfork bitters squid cornhole roof party tattooed truffaut woke.'
+      info={[
+        {label: 'Ice cream', value: 'Vanilla'},
+        {label: 'Juice', value: 'Orange'}
+      ]}
+      accept='Yes, love those flavors'
+      cancel='Chocolate and strawberry please'
+      onAccept={() => console.log('The user likes the flavors')}
+      onCancel={() => console.log('The prefers chocolate')}
+      brandVolume={highBrandVolume ? 'high' : undefined}
+    />
+  </Wrapper>
+
+  return <div
+    style={{
+      marginRight: grid(4),
+      width: `calc(80% - ${grid(4)})`
+    }}>
+    <Canvas
+      dimensions={desktop
+        ? undefined
+        : (narrow
+          ? [280, 500]
+          : [400, 600]
+        )}>
+      {content}
+    </Canvas>
+
+    <Code.Block standalone style={{marginTop: 20}}>
+      {asCode(content)}
+    </Code.Block>
+  </div>
+}
+
+ConfirmDataExample.options = [
+  {
+    label: 'Desktop',
+    name: 'desktop',
+    value: false
+  },
+  {
+    label: 'Narrow',
+    name: 'narrow',
+    value: false
+  },
+  {
+    label: 'High brand volume',
+    name: 'highBrandVolume',
+    value: false
+  }
+]
+
+ConfirmDataExample.defaultProps = {
+  desktop: false,
+  narrow: false,
+  highBrandVolume: false
+}
+
+function ReviewDataExample ({
+  desktop,
+  narrow,
+  highBrandVolume
+}) {
+  const content = <Wrapper
+    layout={desktop
+      ? 'desktop'
+      : (narrow
+        ? 'narrow'
+        : 'mobile'
+      )}
+    onBack={() => console.log('welcome back')}
+    onClose={() => console.log('that was close')}>
+    <ReviewData
+      id='review-data'
+      title='Review the chosen flavors'
+      summary='Street art tattooed live-edge, kitsch four loko hashtag paleo banh mi art party. Viral flexitarian paleo, stumptown dreamcatcher ennui pitchfork bitters squid cornhole roof party tattooed truffaut woke.'
+      info={[
+        {label: 'Ice cream', value: 'Vanilla'},
+        {label: 'Juice', value: 'Orange & Pineapple'}
+      ]}
+      legal='Forage vinyl gluten-free drinking vinegar activated charcoal, listicle shoreditch pitchfork biodiesel venmo organic blue bottle truffaut helvetica. Air plant locavore typewriter, post-ironic banjo leggings sriracha scenester microdosing fanny pack readymade squid raclette stumptown neutra.'
+      accept='Yes, awesome'
+      onAccept={() => console.log('The user thinks the flavors are awesome')}
+      brandVolume={highBrandVolume ? 'high' : undefined}
+    />
+  </Wrapper>
+
+  return <div
+    style={{
+      marginRight: grid(4),
+      width: `calc(80% - ${grid(4)})`
+    }}>
+    <Canvas
+      dimensions={desktop
+        ? undefined
+        : (narrow
+          ? [280, 500]
+          : [400, 600]
+        )}>
+      {content}
+    </Canvas>
+
+    <Code.Block standalone style={{marginTop: 20}}>
+      {asCode(content)}
+    </Code.Block>
+  </div>
+}
+
+ReviewDataExample.options = [
+  {
+    label: 'Desktop',
+    name: 'desktop',
+    value: false
+  },
+  {
+    label: 'Narrow',
+    name: 'narrow',
+    value: false
+  },
+  {
+    label: 'High brand volume',
+    name: 'highBrandVolume',
+    value: false
+  }
+]
+
+ReviewDataExample.defaultProps = {
+  desktop: false,
+  narrow: false,
+  highBrandVolume: false
+}
+
 export default {
   title: 'Informative',
 
@@ -209,261 +468,45 @@ export default {
       title: 'Landing',
       type: SHOWCASE,
 
-      example: React.createElement(lekplats(LandingExample.options)(LandingExample))
+      example: React.createElement(
+        lekplats(LandingExample.options)(LandingExample)
+      )
     },
 
     {
       title: 'Explanation',
       type: SHOWCASE,
 
-      example: React.createElement(lekplats(ExplanationExample.options)(ExplanationExample))
+      example: React.createElement(
+        lekplats(ExplanationExample.options)(ExplanationExample)
+      )
     },
-    //
-    // {
-    //   title: 'Landing',
-    //   require: 'import Landing from \'@klarna/ui/templates/Landing\'',
-    //   type: LIVE,
-    //
-    //   examples: {
-    // },
-    //
-    // {
-    //   title: 'Explanation',
-    //   require: 'import Explanation from \'@klarna/ui/templates/Explanation\'',
-    //   type: LIVE,
-    //
-    //   examples: {
-    //     Regular: <div
-    //       style={{
-    //         boxSizing: 'border-box',
-    //         borderWidth: grid(0.2),
-    //         borderStyle: 'solid',
-    //         borderColor: palette.GREY_LINES
-    //       }}>
-    //       <Wrapper
-    //         fixed
-    //         onBack={() => console.log('welcome back')}>
-    //         <Explanation
-    //           id='explanation'
-    //           title='This is how the product works'
-    //           content='Chia williamsburg subway tile vaporware, live-edge kinfolk cardigan prism deep v retro seitan.'
-    //           legal='Drinking vinegar unicorn fam pork belly prism. Vegan bicycle rights raclette tofu squid lomo coloring book, meggings marfa PBR&B bushwick. '
-    //         />
-    //       </Wrapper>
-    //     </div>
-    //   }
-    // },
-    //
-    // {
-    //   title: 'XStepExplanation',
-    //   require: `import XStepExplanation from '@klarna/ui/templates/XStepExplanation'`,
-    //   type: LIVE,
-    //
-    //   examples: {
-    //     Regular: <div
-    //       style={{
-    //         boxSizing: 'border-box',
-    //         borderWidth: grid(0.2),
-    //         borderStyle: 'solid',
-    //         borderColor: palette.GREY_LINES
-    //       }}>
-    //       <Wrapper
-    //         fixed
-    //         onBack={() => console.log('welcome back')}
-    //         onClose={() => console.log('that was close')}>
-    //         <XStepExplanation
-    //           id='x-step-explanation'
-    //           title='This is the main feature headline'
-    //           accept='Continue'
-    //           bullets={[
-    //             {
-    //               icon: <svg
-    //                 width='60px'
-    //                 height='60px'
-    //                 viewBox='0 0 60 60'>
-    //                 <circle
-    //                   stroke='#979797'
-    //                   fill='#D8D8D8'
-    //                   cx='30'
-    //                   cy='30'
-    //                   r='28'
-    //                 />
-    //               </svg>,
-    //               content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
-    //             },
-    //             {
-    //               icon: <svg
-    //                 width='60px'
-    //                 height='60px'
-    //                 viewBox='0 0 60 60'>
-    //                 <polygon
-    //                   stroke='#979797'
-    //                   fill='#D8D8D8'
-    //                   points='30 46.5 12.95 55.46 16.20 36.48 2.41 23.03 21.47 20.26 30 3 38.52 20.26 57.58 23.03 43.79 36.48 47.04 55.46'
-    //                 />
-    //               </svg>,
-    //               content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
-    //             },
-    //             {
-    //               icon: <svg
-    //                 width='60px'
-    //                 height='60px'
-    //                 viewBox='0 0 60 60'>
-    //                 <polygon
-    //                   stroke='#979797'
-    //                   fill='#D8D8D8'
-    //                   points='30 4 58 54 2 54'
-    //                 />
-    //               </svg>,
-    //               content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
-    //             }
-    //           ]}
-    //           onAccept={() => console.log('Continue')}
-    //         />
-    //       </Wrapper>
-    //     </div>,
-    //
-    //     'With high brand volume': <div
-    //       style={{
-    //         boxSizing: 'border-box',
-    //         borderWidth: grid(0.2),
-    //         borderStyle: 'solid',
-    //         borderColor: palette.GREY_LINES
-    //       }}>
-    //       <Wrapper
-    //         fixed
-    //         onBack={() => console.log('welcome back')}
-    //         onClose={() => console.log('that was close')}>
-    //         <XStepExplanation
-    //           brandVolume='high'
-    //           title='This is the main feature headline'
-    //           accept='Continue'
-    //           bullets={[
-    //             {
-    //               icon: <svg
-    //                 width='60px'
-    //                 height='60px'
-    //                 viewBox='0 0 60 60'>
-    //                 <circle
-    //                   stroke='#979797'
-    //                   fill='#D8D8D8'
-    //                   cx='30'
-    //                   cy='30'
-    //                   r='28'
-    //                 />
-    //               </svg>,
-    //               content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
-    //             },
-    //             {
-    //               icon: <svg
-    //                 width='60px'
-    //                 height='60px'
-    //                 viewBox='0 0 60 60'>
-    //                 <polygon
-    //                   stroke='#979797'
-    //                   fill='#D8D8D8'
-    //                   points='30 46.5 12.95 55.46 16.20 36.48 2.41 23.03 21.47 20.26 30 3 38.52 20.26 57.58 23.03 43.79 36.48 47.04 55.46'
-    //                 />
-    //               </svg>,
-    //               content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
-    //             },
-    //             {
-    //               icon: <svg
-    //                 width='60px'
-    //                 height='60px'
-    //                 viewBox='0 0 60 60'>
-    //                 <polygon
-    //                   stroke='#979797'
-    //                   fill='#D8D8D8'
-    //                   points='30 4 58 54 2 54'
-    //                 />
-    //               </svg>,
-    //               content: 'Maecenas in dui ut tortor tristique auctor consequat et tortor. Sed cursus malesuada tellus dapibus.'
-    //             }
-    //           ]}
-    //           onAccept={() => console.log('Continue')}
-    //         />
-    //       </Wrapper>
-    //     </div>
-    //   }
-    // },
-    //
-    // {
-    //   title: 'ConfirmData',
-    //   require: 'import ConfirmData from \'@klarna/ui/templates/ConfirmData\'',
-    //   type: TEMPLATE,
-    //
-    //   examples: {
-    //     Regular: {
-    //       inline: <ConfirmData
-    //         id='confirm-data'
-    //         title='Are these your favorite flavors?'
-    //         summary='Street art tattooed live-edge, kitsch four loko hashtag paleo banh mi art party. Viral flexitarian paleo, stumptown dreamcatcher ennui pitchfork bitters squid cornhole roof party tattooed truffaut woke.'
-    //         info={[
-    //           {label: 'Ice cream', value: 'Vanilla'},
-    //           {label: 'Juice', value: 'Orange'}
-    //         ]}
-    //         accept='Yes, love those flavors'
-    //         cancel='Chocolate and strawberry please'
-    //         onAccept={() => console.log('The user likes the flavors')}
-    //         onCancel={() => console.log('The prefers chocolate')}
-    //       />
-    //     },
-    //
-    //     'With high brand volume': {
-    //       inline: <ConfirmData
-    //         brandVolume='high'
-    //         title='Are these your favorite flavors?'
-    //         summary='Street art tattooed live-edge, kitsch four loko hashtag paleo banh mi art party. Viral flexitarian paleo, stumptown dreamcatcher ennui pitchfork bitters squid cornhole roof party tattooed truffaut woke.'
-    //         info={[
-    //           {label: 'Ice cream', value: 'Vanilla'},
-    //           {label: 'Juice', value: 'Orange'}
-    //         ]}
-    //         accept='Yes, love those flavors'
-    //         cancel='Chocolate and strawberry please'
-    //         onAccept={() => console.log('The user likes the flavors')}
-    //         onCancel={() => console.log('The prefers chocolate')}
-    //       />
-    //     }
-    //   }
-    // },
-    //
-    // {
-    //   title: 'ReviewData',
-    //   require: 'import ReviewData from \'@klarna/ui/templates/ReviewData\'',
-    //   type: TEMPLATE,
-    //
-    //   examples: {
-    //     Regular: {
-    //       inline: <ReviewData
-    //         id='review-data'
-    //         title='Review the chosen flavors'
-    //         summary='Street art tattooed live-edge, kitsch four loko hashtag paleo banh mi art party. Viral flexitarian paleo, stumptown dreamcatcher ennui pitchfork bitters squid cornhole roof party tattooed truffaut woke.'
-    //         info={[
-    //           {label: 'Ice cream', value: 'Vanilla'},
-    //           {label: 'Juice', value: 'Orange & Pineapple'}
-    //         ]}
-    //         legal='Forage vinyl gluten-free drinking vinegar activated charcoal, listicle shoreditch pitchfork biodiesel venmo organic blue bottle truffaut helvetica. Air plant locavore typewriter, post-ironic banjo leggings sriracha scenester microdosing fanny pack readymade squid raclette stumptown neutra.'
-    //         accept='Yes, awesome'
-    //         onAccept={() => console.log('The user thinks the flavors are awesome')}
-    //       />
-    //     },
-    //
-    //     'With high brand volume': {
-    //       inline: <ReviewData
-    //         brandVolume='high'
-    //         title='Review the chosen flavors'
-    //         summary='Street art tattooed live-edge, kitsch four loko hashtag paleo banh mi art party. Viral flexitarian paleo, stumptown dreamcatcher ennui pitchfork bitters squid cornhole roof party tattooed truffaut woke.'
-    //         info={[
-    //           {label: 'Ice cream', value: 'Vanilla'},
-    //           {label: 'Juice', value: 'Orange & Pineapple'}
-    //         ]}
-    //         legal='Forage vinyl gluten-free drinking vinegar activated charcoal, listicle shoreditch pitchfork biodiesel venmo organic blue bottle truffaut helvetica. Air plant locavore typewriter, post-ironic banjo leggings sriracha scenester microdosing fanny pack readymade squid raclette stumptown neutra.'
-    //         accept='Yes, awesome'
-    //         onAccept={() => console.log('The user thinks the flavors are awesome')}
-    //       />
-    //     }
-    //   }
-    // }
+
+    {
+      title: 'XStepExplanation',
+      type: SHOWCASE,
+
+      example: React.createElement(
+        lekplats(XStepExplanationExample.options)(XStepExplanationExample)
+      )
+    },
+
+    {
+      title: 'ConfirmData',
+      type: SHOWCASE,
+
+      example: React.createElement(
+        lekplats(ConfirmDataExample.options)(ConfirmDataExample)
+      )
+    },
+
+    {
+      title: 'ReviewData',
+      type: SHOWCASE,
+
+      example: React.createElement(
+        lekplats(ReviewDataExample.options)(ReviewDataExample)
+      )
+    }
   ]
 }
