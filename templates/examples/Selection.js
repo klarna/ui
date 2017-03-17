e326c164af060f116d4322b4e262113b3c49092e
Fernando Via Canel <fernando.canel@klarna.com>
2017-03-17 15:06:23 +0100

    Update all Selection examples

--- templates/examples/Selection.js
+++ templates/examples/Selection.js
@@ -6,282 +6,9 @@ import Wrapper from '../Wrapper'
 import DemoIcon from './DemoIcon'
 import {SHOWCASE} from '../../Showroom/variationTypes'
 
-const optionsData = [
-  {key: 'lor', label: 'Lorem'},
-  {key: 'ip', label: 'Ipsum'}
-]
-
-import Canvas from '../../Showroom/Lekplats/Canvas'
-import lekplats from '../../Showroom/Lekplats'
-import * as Code from '../../Code'
-import asCode from '../../helpers/asCode'
+import Lekplats from '../../Showroom/Lekplats'
 
 import grid from '../../settings/grid'
-import * as palette from '../../settings/palette'
-
-function SelectionExample ({
-  desktop,
-  narrow,
-  visible
-}) {
-  const content = <Wrapper
-    layout={desktop
-      ? 'desktop'
-      : (narrow
-        ? 'narrow'
-        : 'mobile'
-      )}
-    visible={visible}
-    onBack={() => console.log('welcome back')}
-    onClose={() => console.log('welcome back')}>
-    <Selection
-      id='selection'
-      title='What option do you prefer?'
-      summary='Chia williamsburg subway tile vaporware, live-edge kinfolk cardigan prism deep v retro seitan.'
-      options={optionsData}
-      value='ip'
-      onSelect={(key) => console.log('selected ' + key)}
-    />
-  </Wrapper>
-
-  return <div
-    style={{
-      marginRight: grid(4),
-      width: `calc(80% - ${grid(4)})`
-    }}>
-    <Canvas
-      dimensions={desktop
-        ? undefined
-        : (narrow
-          ? [280, 500]
-          : [400, 600]
-        )}>
-      {content}
-    </Canvas>
-
-    <Code.Block standalone style={{marginTop: 20}}>
-      {asCode(content)}
-    </Code.Block>
-  </div>
-}
-
-SelectionExample.options = [
-  {
-    label: 'Desktop',
-    name: 'desktop',
-    value: false
-  },
-  {
-    label: 'Narrow',
-    name: 'narrow',
-    value: false
-  },
-  {
-    label: 'Visible',
-    name: 'visible',
-    value: true
-  }
-]
-
-SelectionExample.defaultProps = {
-  desktop: false,
-  narrow: false,
-  visible: false
-}
-
-function FilteredSelectionExample ({
-  desktop,
-  narrow,
-  uncontrolled,
-  visible
-}) {
-  const filteredSelectionExample = uncontrolled
-    ? <FilteredSelection
-      id='filtered-selection'
-      title='Select your flavor'
-      summary='Photo booth distillery man bun, bitters stumptown freegan cliche cronut green juice.'
-      label='Start typing a flavor'
-      options={[
-        {key: 'chocolate', label: 'Chocolate'},
-        {key: 'vanilla', label: 'Vanilla'},
-        {key: 'raspberry', label: 'Raspberry'},
-        {key: 'tiramisu', label: 'Tiramisu'},
-        {key: 'mascarpone', label: 'Mascarpone'},
-        {key: 'banana-split', label: 'Banana Split'},
-        {key: 'anana', label: 'Anana'}
-      ]}
-      alternative='Manually enter your favorite flavor'
-      onAlternative={() => console.log('alternative')}
-      onChange={(e) => console.log('new value', e)}
-      onSelect={(e) => console.log('selection', e)}
-      defaultValue='A'
-    />
-    : <FilteredSelection
-      id='filtered-selection'
-      title='Select your flavor'
-      summary='Photo booth distillery man bun, bitters stumptown freegan cliche cronut green juice.'
-      label='Start typing a flavor'
-      options={[
-        {key: 'chocolate', label: 'Chocolate'},
-        {key: 'vanilla', label: 'Vanilla'},
-        {key: 'raspberry', label: 'Raspberry'},
-        {key: 'tiramisu', label: 'Tiramisu'},
-        {key: 'mascarpone', label: 'Mascarpone'},
-        {key: 'banana-split', label: 'Banana Split'},
-        {key: 'anana', label: 'Anana'}
-      ]}
-      alternative='Manually enter your favorite flavor'
-      onAlternative={() => console.log('alternative')}
-      onChange={(e) => console.log('new value', e)}
-      onSelect={(e) => console.log('selection', e)}
-      value='A'
-    />
-
-  const content = <Wrapper
-    layout={desktop
-      ? 'desktop'
-      : (narrow
-        ? 'narrow'
-        : 'mobile'
-      )}
-    visible={visible}
-    onBack={() => console.log('welcome back')}
-    onClose={() => console.log('welcome back')}>
-    {filteredSelectionExample}
-  </Wrapper>
-
-  return <div
-    style={{
-      marginRight: grid(4),
-      width: `calc(80% - ${grid(4)})`
-    }}>
-    <Canvas
-      dimensions={desktop
-        ? undefined
-        : (narrow
-          ? [280, 500]
-          : [400, 600]
-        )}>
-      {content}
-    </Canvas>
-
-    <Code.Block standalone style={{marginTop: 20}}>
-      {asCode(content)}
-    </Code.Block>
-  </div>
-}
-
-FilteredSelectionExample.options = [
-  {
-    label: 'Visible',
-    name: 'visible',
-    value: true
-  },
-  {
-    label: 'Desktop',
-    name: 'desktop',
-    value: false
-  },
-  {
-    label: 'Narrow',
-    name: 'narrow',
-    value: false
-  },
-  {
-    label: 'Uncontrolled',
-    name: 'uncontrolled',
-    value: false
-  }
-]
-
-FilteredSelectionExample.defaultProps = {
-  desktop: false,
-  narrow: false,
-  uncontrolled: false,
-  visible: false
-}
-
-function CenteredSelectionExample ({
-  desktop,
-  narrow,
-  uncontrolled,
-  visible
-}) {
-  const content = <Wrapper
-    layout={desktop
-      ? 'desktop'
-      : (narrow
-        ? 'narrow'
-        : 'mobile'
-      )}
-    visible={visible}
-    onBack={() => console.log('welcome back')}
-    onClose={() => console.log('welcome back')}>
-    <CenteredSelection
-      id='centered-selection'
-      illustration={<DemoIcon />}
-      title='Select your flavor'
-      summary='Photo booth distillery man bun, bitters stumptown freegan cliche cronut green juice.'
-      options={[
-        {
-          key: 'chocolate',
-          description: 'Chocolate ice cream with cookies & swirls of pudding ice cream',
-          label: 'Chocolate'
-        },
-        {
-          key: 'vanilla',
-          description: 'Dark caramel & vanilla ice creams with cookies & butter swirl',
-          label: 'Vanilla'
-        }
-      ]}
-      onSelect={(key) => console.log('selected ' + key)}
-    />
-  </Wrapper>
-
-  return <div
-    style={{
-      marginRight: grid(4),
-      width: `calc(80% - ${grid(4)})`
-    }}>
-    <Canvas
-      dimensions={desktop
-        ? undefined
-        : (narrow
-          ? [280, 500]
-          : [400, 600]
-        )}>
-      {content}
-    </Canvas>
-
-    <Code.Block standalone style={{marginTop: 20}}>
-      {asCode(content)}
-    </Code.Block>
-  </div>
-}
-
-CenteredSelectionExample.options = [
-  {
-    label: 'Visible',
-    name: 'visible',
-    value: true
-  },
-  {
-    label: 'Desktop',
-    name: 'desktop',
-    value: false
-  },
-  {
-    label: 'Narrow',
-    name: 'narrow',
-    value: false
-  }
-]
-
-CenteredSelectionExample.defaultProps = {
-  desktop: false,
-  narrow: false,
-  visible: false
-}
 
 export default {
   title: 'Selection',
@@ -290,27 +17,140 @@ export default {
       title: 'Selection',
       type: SHOWCASE,
 
-      example: React.createElement(
-        lekplats(SelectionExample.options)(SelectionExample)
-      )
+      example: <Lekplats
+        options={[
+          {
+            label: 'Visible',
+            name: 'visible',
+            value: true
+          }
+        ]}>
+        {({visible}) => <Wrapper
+          visible={visible}
+          onBack={() => console.log('welcome back')}
+          onClose={() => console.log('welcome back')}>
+          <Selection
+            id='selection'
+            title='What option do you prefer?'
+            summary='Chia williamsburg subway tile vaporware, live-edge kinfolk cardigan prism deep v retro seitan.'
+            options={[
+              {key: 'lor', label: 'Lorem'},
+              {key: 'ip', label: 'Ipsum'}
+            ]}
+            value='ip'
+            onSelect={(key) => console.log('selected ' + key)}
+          />
+        </Wrapper>}
+      </Lekplats>
     },
 
     {
       title: 'FilteredSelection',
       type: SHOWCASE,
 
-      example: React.createElement(
-        lekplats(FilteredSelectionExample.options)(FilteredSelectionExample)
-      )
+      example: <Lekplats
+        options={[
+          {
+            label: 'Visible',
+            name: 'visible',
+            value: true
+          },
+          {
+            label: 'Uncontrolled',
+            name: 'uncontrolled',
+            value: false
+          }
+        ]}>
+        {({uncontrolled, visible}) => {
+          const filteredSelectionExample = uncontrolled
+            ? <FilteredSelection
+              id='filtered-selection'
+              title='Select your flavor'
+              summary='Photo booth distillery man bun, bitters stumptown freegan cliche cronut green juice.'
+              label='Start typing a flavor'
+              options={[
+                {key: 'chocolate', label: 'Chocolate'},
+                {key: 'vanilla', label: 'Vanilla'},
+                {key: 'raspberry', label: 'Raspberry'},
+                {key: 'tiramisu', label: 'Tiramisu'},
+                {key: 'mascarpone', label: 'Mascarpone'},
+                {key: 'banana-split', label: 'Banana Split'},
+                {key: 'anana', label: 'Anana'}
+              ]}
+              alternative='Manually enter your favorite flavor'
+              onAlternative={() => console.log('alternative')}
+              onChange={(e) => console.log('new value', e)}
+              onSelect={(e) => console.log('selection', e)}
+              defaultValue='A'
+            />
+            : <FilteredSelection
+              id='filtered-selection'
+              title='Select your flavor'
+              summary='Photo booth distillery man bun, bitters stumptown freegan cliche cronut green juice.'
+              label='Start typing a flavor'
+              options={[
+                {key: 'chocolate', label: 'Chocolate'},
+                {key: 'vanilla', label: 'Vanilla'},
+                {key: 'raspberry', label: 'Raspberry'},
+                {key: 'tiramisu', label: 'Tiramisu'},
+                {key: 'mascarpone', label: 'Mascarpone'},
+                {key: 'banana-split', label: 'Banana Split'},
+                {key: 'anana', label: 'Anana'}
+              ]}
+              alternative='Manually enter your favorite flavor'
+              onAlternative={() => console.log('alternative')}
+              onChange={(e) => console.log('new value', e)}
+              onSelect={(e) => console.log('selection', e)}
+              value='A'
+            />
+
+          return <Wrapper
+            visible={visible}
+            onBack={() => console.log('welcome back')}
+            onClose={() => console.log('welcome back')}>
+            {filteredSelectionExample}
+          </Wrapper>
+        }}
+      </Lekplats>
     },
 
     {
       title: 'CenteredSelection',
       type: SHOWCASE,
 
-      example: React.createElement(
-        lekplats(CenteredSelectionExample.options)(CenteredSelectionExample)
-      )
+      example: <Lekplats
+        options={[
+          {
+            label: 'Visible',
+            name: 'visible',
+            value: true
+          }
+        ]}>
+        {({visible}) => <Wrapper
+          visible={visible}
+          onBack={() => console.log('welcome back')}
+          onClose={() => console.log('welcome back')}>
+          <CenteredSelection
+            id='centered-selection'
+            illustration={<DemoIcon />}
+            title='Select your flavor'
+            summary='Photo booth distillery man bun, bitters stumptown freegan cliche cronut green juice.'
+            options={[
+              {
+                key: 'chocolate',
+                description: 'Chocolate ice cream with cookies & swirls of pudding ice cream',
+                label: 'Chocolate'
+              },
+              {
+                key: 'vanilla',
+                description: 'Dark caramel & vanilla ice creams with cookies & butter swirl',
+                label: 'Vanilla'
+              }
+            ]}
+            onSelect={(key) => console.log('selected ' + key)}
+          />
+        </Wrapper>}
+      </Lekplats>
     }
   ]
 }
