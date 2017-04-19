c2cec927a7bb8d5d6725495a78598afdb6a6d2fa
Brandon Every <brandon.every@klarna.com>
2017-04-19 16:21:19 -0400

    Add FieldLink Component

---
+++ FieldLink/index.jsx
@@ -0,0 +1,35 @@
+import React, { PropTypes } from 'react'
+import classNamesBind from 'classnames/bind'
+import defaultStyles from './styles.scss'
+
+const baseClass = 'field-link'
+
+class FieldLink extends React.Component {
+  propTypes: {
+    id: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    onFieldLinkClick: PropTypes.func
+  }
+
+  render () {
+    const {
+      id,
+      label,
+      onFieldLinkClick
+    } = this.props
+
+    const classNames = classNamesBind.bind({ ...defaultStyles })
+
+    return (
+      <a
+        href="#"
+        id={id}
+        onClick={onFieldLinkClick}
+        className={classNames(baseClass)}>
+        {label}
+      </a>
+    )
+  }
+}
+
+export default FieldLink
