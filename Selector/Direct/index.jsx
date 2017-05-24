c0b8b395df56231ec943fe2e4a6917f3a192cbd9
Fernando Via Canel <fernando.canel@klarna.com>
2017-05-24 17:28:31 +0200

    Make Selector.Direct into a function

--- Selector/Direct/index.jsx
+++ Selector/Direct/index.jsx
@@ -14,78 +14,76 @@ const classes = {
   description: `${baseClass}__description`
 }
 
-export default React.createClass({
-  displayName: 'Selector.Direct',
+function Direct ({
+  className,
+  data,
+  id,
+  onSelect,
+  styles,
+  ...remainingProps
+}) {
+  const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
+  const ids = id
+    ? {
+      description: (key) => `${id}__${key}__description`,
+      icon: (key) => `${id}__${key}__icon`,
+      label: (key) => `${id}__${key}__label`,
+      option: (key) => `${id}__${key}__option`
+    } : {
+      description: () => {},
+      icon: () => {},
+      label: () => {},
+      option: () => {}
+    }
 
-  propTypes: {
-    className: PropTypes.string,
-    data: PropTypes.array.isRequired,
-    id: PropTypes.string,
-    onSelect: PropTypes.func,
-    styles: PropTypes.object
-  },
+  return (
+    <div
+      className={classNames(baseClass, 'title', className)}
+      id={id}
+      {...remainingProps}>
+      {data.map(({ key, label, description }) => [
+        <a
+          href={`#${key}`}
+          id={ids.option(key)}
+          onClick={(e) => {
+            e.preventDefault()
+            onSelect && onSelect(key)
+          }}
+          className={classNames(classes.item)}
+          key={key}>
+          <div
+            className={classNames(classes.label)}
+            id={ids.label(key)}>
+            {label}
+          </div>
+          {
+            description && (
+              <div
+                className={classNames(classes.description)}
+                id={ids.description(key)}>
+                {description}
+              </div>
+            )
+          }
+          <Right
+            className={classNames(classes.icon)}
+            id={ids.icon(key)}
+            color='black'
+          />
+        </a>
+      ])}
+    </div>
+  )
+}
 
-  render () {
-    const {
-      className,
-      data,
-      id,
-      onSelect,
-      styles,
-      ...remainingProps
-    } = this.props
+Direct.propTypes = {
+  className: PropTypes.string,
+  data: PropTypes.array.isRequired,
+  id: PropTypes.string,
+  onSelect: PropTypes.func,
+  styles: PropTypes.object
+}
 
-    const classNames = classNamesBind.bind({ ...defaultStyles, ...styles })
-    const ids = id
-      ? {
-        description: (key) => `${id}__${key}__description`,
-        icon: (key) => `${id}__${key}__icon`,
-        label: (key) => `${id}__${key}__label`,
-        option: (key) => `${id}__${key}__option`
-      } : {
-        description: () => {},
-        icon: () => {},
-        label: () => {},
-        option: () => {}
-      }
+Direct.displayName = 'Selector.Direct'
 
-    return (
-      <div
-        className={classNames(baseClass, 'title', className)}
-        id={id}
-        {...remainingProps}>
-        {data.map(({ key, label, description }) => [
-          <a
-            href={`#${key}`}
-            id={ids.option(key)}
-            onClick={(e) => {
-              e.preventDefault()
-              onSelect && onSelect(key)
-            }}
-            className={classNames(classes.item)}
-            key={key}>
-            <div
-              className={classNames(classes.label)}
-              id={ids.label(key)}>
-              {label}
-            </div>
-            {
-              description && (
-                <div
-                  className={classNames(classes.description)}
-                  id={ids.description(key)}>
-                  {description}
-                </div>
-              )
-            }
-            <Right
-              className={classNames(classes.icon)}
-              id={ids.icon(key)}
-              color='black'
-            />
-          </a>
-        ])}
-      </div>
-    )
-  }
-})
+export default Direct
