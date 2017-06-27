cdb753d1964af8cae0e8072cd6437e3aba594065
Brandon Every <brandon.every@klarna.com>
2017-06-27 12:09:23 -0400

    New styles

--- TermsBanner/demoLogo.jsx
+++ TermsBanner/demoLogo.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 export default function DemoLogo () {
   const styles = {
     height: '14px',
-    transform: 'scale(1.3)'
+    transform: 'scale(1.3)',
+    'transform-origin': '0 50%'
   }
 
   return <svg viewBox='0 0 75 20' version='1.1' style={styles}>
     <g>
-      <text stroke='#8e8e90' id='svg_1' y='16' x='15' fill='#8e8e90'>ABCDEF</text>
+      <text stroke='#8e8e90' id='svg_1' y='16' x='0' fill='#8e8e90'>ABCDEF</text>
     </g>
   </svg>
 }
