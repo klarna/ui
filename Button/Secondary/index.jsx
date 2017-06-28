58609f6175f809dc6acce4469b189b5da2c0f9fe
Brandon Every <brandon.every@klarna.com>
2017-06-28 19:51:32 -0400

    Update index.jsx

--- Button/Secondary/index.jsx
+++ Button/Secondary/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import compose from 'ramda/src/compose'
-import {overridable, withTheme} from '@klarna/higher-order-components'
+import {overridable, themeable} from '@klarna/higher-order-components'
 import { withPropsFromContext } from 'react-context-props'
 import classNamesBind from 'classnames/bind'
 import parseColor from 'parse-color'
@@ -168,7 +168,7 @@ Secondary.propTypes = {
 
 export default compose(
   withPropsFromContext(['brandVolume']),
-  withTheme((customizations, { customize }) => ({
+  themeable((customizations, { customize }) => ({
     customize: {
       backgroundColor: customizations.color_button,
       borderRadius: customizations.radius_border,
