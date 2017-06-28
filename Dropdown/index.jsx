e73e32d55b66bc72f5c94f3e116f85e8c7b82fab
Brandon Every <brandon.every@klarna.com>
2017-06-28 19:53:44 -0400

    Update index.jsx

--- Dropdown/index.jsx
+++ Dropdown/index.jsx
@@ -12,9 +12,9 @@ import FieldTooltip from '../FieldTooltip'
 import compose from 'ramda/src/compose'
 import {
   overridable,
-  withTheme,
-  withUncontrolledProp,
-  withUniqueFormIdentifier
+  themeable,
+  uncontrolled,
+  uniqueName
 } from '@klarna/higher-order-components'
 
 const baseClass = 'dropdown'
