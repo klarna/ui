6e409c82a726345d74b693645c35b0ca26febefe
Brandon Every <brandon.every@klarna.com>
2017-08-24 17:16:39 -0400

    Update index.jsx

--- TermsBanner/index.jsx
+++ TermsBanner/index.jsx
@@ -23,7 +23,10 @@ function TermsBanner ({
   const ids = id
     ? {
       link: `${id}__link`,
-      altLink: `${id}__alt-link`
+      altLink: `${id}__alt-link`,
+      logoWrapper: `${id}__logo-wrapper`,
+      altLinkWrapper: `${id}__alt-link-wrapper`,
+      linkWrapper: `${id}__link-wrapper`
     } : {}
 
   return (
@@ -32,20 +35,26 @@ function TermsBanner ({
       className={classNames(baseClass)}
       {...props}
     >
-      {logo}
-      {onAltLinkClick && <span
-        id={ids.altLink}
-        className={classNames(classes.altLink)}
-        onClick={onAltLinkClick}>
-        {altLink}
-        </span>
+      <div id={ids.logoWrapper}>
+        {logo}
+      </div>
+      {onAltLinkClick && <div id={ids.altLinkWrapper}>
+          <span
+            id={ids.altLink}
+            className={classNames(classes.altLink)}
+            onClick={onAltLinkClick}>
+            {altLink}
+          </span>
+        </div>
       }
-      {onLinkClick && <span
-        id={ids.link}
-        className={classNames(classes.link)}
-        onClick={onLinkClick}>
-        {link}
-        </span>
+      {onLinkClick && <div id={ids.linkWrapper}>
+          <span
+            id={ids.link}
+            className={classNames(classes.link)}
+            onClick={onLinkClick}>
+            {link}
+          </span>
+        </div>
       }
     </div>
   )
